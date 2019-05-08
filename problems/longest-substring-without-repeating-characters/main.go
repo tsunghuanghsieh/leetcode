bea3d4bbc7e5fa8b266d5761b1a6031a85d9ce64
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2019-05-08 15:15:46 -0700

    Use consistent indentations.

--- problems/longest-substring-without-repeating-characters/main.go
+++ problems/longest-substring-without-repeating-characters/main.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-	"fmt"
-	// "strings"
+    "fmt"
+    // "strings"
 )
 
 func main ()  {
-	fmt.Println("length is", lengthOfLongestSubstring("abcabcbb"));
-	// fmt.Println("length is", lengthOfLongestSubstring("bbbbb"));
-	// fmt.Println("length is", lengthOfLongestSubstring("pwwkew"));
-	// fmt.Println("length is", lengthOfLongestSubstring("dvdf"));
-	// fmt.Println("length is", lengthOfLongestSubstring("abba"));
-	// fmt.Println("length is", lengthOfLongestSubstring("abcdefgchijkbcbb"));
+    fmt.Println("length is", lengthOfLongestSubstring("abcabcbb"));
+    // fmt.Println("length is", lengthOfLongestSubstring("bbbbb"));
+    // fmt.Println("length is", lengthOfLongestSubstring("pwwkew"));
+    // fmt.Println("length is", lengthOfLongestSubstring("dvdf"));
+    // fmt.Println("length is", lengthOfLongestSubstring("abba"));
+    // fmt.Println("length is", lengthOfLongestSubstring("abcdefgchijkbcbb"));
 }
 
 func lengthOfLongestSubstring(input string) int {
-	var longest int = 0;
-	var startIdx int = 0;
-	// string in map seems to run faster than byte, however memory usage is worse
-	mapSubstr := make(map[string]int); // latest position (array index + 1) of character in string
+    var longest int = 0;
+    var startIdx int = 0;
+    // string in map seems to run faster than byte, however memory usage is worse
+    mapSubstr := make(map[string]int); // latest position (array index + 1) of character in string
 
-	for pos := 0; pos < len(input); pos++ {
+    for pos := 0; pos < len(input); pos++ {
         if (mapSubstr[string(input[pos])] != 0 && mapSubstr[string(input[pos])] > startIdx) {
-			// we have seen this character the current substring
-			startIdx = mapSubstr[string(input[pos])];
-			// fmt.Println("startIdx ", startIdx);
-		}
-		mapSubstr[string(input[pos])] = pos + 1;
+            // we have seen this character the current substring
+            startIdx = mapSubstr[string(input[pos])];
+            // fmt.Println("startIdx ", startIdx);
+        }
+        mapSubstr[string(input[pos])] = pos + 1;
 
-		if (longest < pos + 1 - startIdx) {
-			longest = pos + 1 - startIdx;
-		}
-	}
-	return longest;
+        if (longest < pos + 1 - startIdx) {
+            longest = pos + 1 - startIdx;
+        }
+    }
+    return longest;
 }
