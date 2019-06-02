89f37f45261e9f87e8246be22c4867f19c583372
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2019-06-01 19:12:20 -0700

    Handle special cases where k is 0 and negative, as well as 0 in array. Added more conditional checking to for some bug fixes.

--- problems/continuous-subarray-sum/index.js
+++ problems/continuous-subarray-sum/index.js
@@ -5,23 +5,31 @@
  */
 var checkSubarraySum = function(nums, k) {
     var currSum = 0; // current sum of numbers
-    var count = 0;
     var sums = new Map(); // count of current sum in array
     var result = false;
+    var modulo, element_modulo;
+
+    // make k positive
+    k = Math.abs(k);
+
 
     for (var i = 0; i < nums.length; i++) {
         currSum += nums[i];
-
-        if (currSum != nums[i] && currSum % k == 0) {
+        modulo = (k == 0) ? currSum : currSum % k;
+        element_modulo = (k == 0) ? nums[i] : k;
+        if (i > 0 && modulo == 0) {
             return true;
         }
 
-        if (nums[i] != k && sums.has(currSum % k)) {
-            return true;
+        if (sums.has(modulo)) {
+            if ((nums[i] % element_modulo != 0 && nums[i] != 0) ||
+                (k == 0 && i > 0 && nums[i - 1] == 0)) { // k = 0 and [1,0,0], [0,1,0], [0,0,1]
+                return true;
+            }
         }
 
         // count current sum in hashmap
-        sums.set(currSum % k, 1);
+        sums.set(modulo, 1);
     }
 
     return result;
