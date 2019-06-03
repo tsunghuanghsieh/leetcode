909b7057906a578f290e6c35e6cb93933ac81039
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2019-06-03 04:47:21 -0700

    Removed obsolete code.

--- problems/frog-jump/index.js
+++ problems/frog-jump/index.js
@@ -13,32 +13,17 @@ var canCross = function(stones) {
         mapStones.set(stones[i], new Set());
     }
 
-    // var queue = [];
-    // key: position
-    // value: k
-    // var start = new Jump(0, 1);
     var landings = [];
     var distances = [];
     landings.push(0);
     distances.push(1);
-    // queue.push(start);
-    // while (queue.length > 0) {
     while (landings.length > 0) {
-        // console.log("index " + mapStones.get(key) + " position " + key);
-        // var current = queue.shift();
-        // var next;
-        // var k = current.k;
-        // var position = current.position;
         var k = distances.shift();
         var position = landings.shift();
-        // console.log("working on [" + position + "," + k + "]");
         for (var i = k - 1; i < k + 2; i++) {
             if (i <= 0) continue;
 
             if (mapStones.has(position + i)) {
-                // if (!mapStonesKeys.has(position + i + '_' + i)) {
-                //     mapStonesKeys.set(position + i + '_' + i, 1);
-                    // queue.push(next);
                 if (!mapStones.get(position + i).has(i)) {
                     mapStones.get(position + i).add(i);
                     landings.push((position + i));
