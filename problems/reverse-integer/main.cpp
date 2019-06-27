dc998920765b80d16573f6ee445ee9d1d703a4ad
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2019-06-26 17:15:53 -0700

    Optimized to use constant O(1) space instead of O(n) where n is the number of digits in INT32_MAX or INT32_MIN. This also drastically simplifies overflow detection logic of reversed number.

--- problems/reverse-integer/main.cpp
+++ problems/reverse-integer/main.cpp
@@ -1,53 +1,26 @@
 #include <iostream>
-#include <math.h>
-#include <vector>
 using namespace std;
 
 class Solution {
 public:
     int reverse(int x) {
+        int remainder = 0;
         int reversed = 0;
-        bool negative = x < 0;
-        vector<int> num, vmax, vmin;
-        int overflow_max = INT32_MAX;
-        int overflow_min = INT32_MIN;
-        bool bMayOverflow = true;
-
-        if (x == 0) return x;
-
         // INT32_MAX is  2147483647 (10 digits)
         // INT32_MIN is -2147483648 (10 digits)
-        while (overflow_max / 10 != 0) {
-            vmax.push_back(overflow_max % 10);
-            vmin.push_back(abs(overflow_min % 10));
-            overflow_max /= 10;
-            overflow_min /= 10;
-        }
-        vmax.push_back(overflow_max);
-        vmin.push_back(abs(overflow_min));
+        int int32_max_remainder = INT32_MAX % 10;
+        int int32_min_remainder = INT32_MIN % 10;
 
-        while (x % 10 == 0) {
-            x /= 10;
-        }
-        while (x / 10 != 0) {
-            num.push_back(abs(x % 10));
-            x /= 10;
-        }
-        num.push_back(abs(x));
-        for (int i = num.size() - 1; i >= 0; i--) {
-            if (num.size() == 10 && bMayOverflow) {
-                // overflow check
-                if (!negative) {
-                    if (num[num.size() - 1 - i] > vmax[i]) return 0;
-                    if (num[num.size() - 1 - i] < vmax[i]) bMayOverflow = false;
-                } else {
-                    if (num[num.size() - 1 - i] > vmin[i]) return 0;
-                    if (num[num.size() - 1 - i] < vmin[i]) bMayOverflow = false;
-                }
+        while (x != 0) {
+            remainder = x % 10;
+            if (reversed > INT32_MAX / 10 || (reversed == INT32_MAX / 10 && remainder > int32_max_remainder) ||
+                reversed < INT32_MIN / 10 || (reversed == INT32_MIN / 10 && remainder < int32_min_remainder)) {
+                return 0;
             }
-            reversed += (num[num.size() - 1 - i] * pow(10, i));
+            reversed = reversed * 10 + remainder;
+            x /= 10;
         }
-        return reversed * ((negative) ? -1 : 1);
+        return reversed;
     }
 };
 
