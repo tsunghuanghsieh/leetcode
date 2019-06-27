3528f74b4df071b124884db3b1ab530ecdd04cac
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2019-06-26 17:55:55 -0700

    Misc changes.

--- problems/reverse-integer/main.cpp
+++ problems/reverse-integer/main.cpp
@@ -6,15 +6,13 @@ public:
     int reverse(int x) {
         int remainder = 0;
         int reversed = 0;
-        // INT32_MAX is  2147483647 (10 digits)
-        // INT32_MIN is -2147483648 (10 digits)
-        int int32_max_remainder = INT32_MAX % 10;
-        int int32_min_remainder = INT32_MIN % 10;
 
         while (x != 0) {
             remainder = x % 10;
-            if (reversed > INT32_MAX / 10 || (reversed == INT32_MAX / 10 && remainder > int32_max_remainder) ||
-                reversed < INT32_MIN / 10 || (reversed == INT32_MIN / 10 && remainder < int32_min_remainder)) {
+            // INT32_MAX is  2147483647 (10 digits)
+            // INT32_MIN is -2147483648 (10 digits)
+            if (reversed > INT32_MAX / 10 || (reversed == INT32_MAX / 10 && remainder > INT32_MAX % 10) ||
+                reversed < INT32_MIN / 10 || (reversed == INT32_MIN / 10 && remainder < INT32_MIN % 10)) {
                 return 0;
             }
             reversed = reversed * 10 + remainder;
