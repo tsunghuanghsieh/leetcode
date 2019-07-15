05d7c9269921ebe81eac1e2851862c5dfe3504cb
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2019-07-15 00:40:24 -0700

    Improved runtime complexity. Improved runtime complexity from O(n log n), where n is the number of elements in intervals, to O(log m) where m is the number of disjoint intervals. Store start time as key and end time as mapped value in std::map(), instead of using std::sort(). Update end time in the map when overlap is encountered.

--- problems/merge-intervals/main.cpp
+++ problems/merge-intervals/main.cpp
@@ -1,36 +1,77 @@
 #include <iostream>
-#include <utility>
+#include <map>
 #include <vector>
 using namespace std;
 
 class Solution {
 private:
-    static bool sortVector(vector<int> v1, vector<int> v2) {
-        return v1[0] < v2[0];
-    };
+    // mapIntervals: map of intervals added so far
+    // start: start time of the current interval
+    // end: end time of the current interval
+    static void mergeOverlapped(map<int, int> &mapIntervals, int start, int end) {
+        auto itr = mapIntervals.upper_bound(start);
+        while (itr != mapIntervals.end()) {
+            // cout << "mergeOverlapped start " << start << " end " << end << " itr->first " << itr->first << " itr->second" << itr->second << endl;
+            if (itr->first > end) break; // no overlap, we are done.
+            if (mapIntervals[start] < itr->second) { // terminating end
+                mapIntervals[start] = end = itr->second;
+            }
+            int temp = itr->first;
+            itr = next(itr);
+            // only delete element in map after iterator has been advanced
+            // or it sets the iterator to the first element in the map
+            mapIntervals.erase(temp);
+        }
+    }
+
+    // static void printMapIntervals(map<int, int> &mapIntervals) {
+    static void printMapIntervals(map<int, int> &mapIntervals) {
+        auto itr = mapIntervals.begin();
+        while (itr != mapIntervals.end()) {
+            cout << "printing current [" << itr->first << "," << itr->second << "]" << endl;
+            itr = next(itr);
+        }
+    }
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int size = intervals.size();
+        map<int, int> mapIntervals;
         vector<vector<int>> result;
 
         // special cases
         if (intervals.empty() || intervals.size() == 0) return result;
 
-        // incurring O(nlogn) runtime complexity
-        sort(intervals.begin(), intervals.end(), sortVector);
         int start = intervals[0][0], end = intervals[0][1];
-        for (int i = 1; i < size; i++) {
-            if (intervals[i][0] > end) {
-                // disjoint
-                result.push_back({start, end});
+
+        mapIntervals[intervals[0][0]] = intervals[0][1];
+        for (int i = 1; i < intervals.size(); i++) {
+            auto itr = mapIntervals.find(intervals[i][0]);
+            if (itr != mapIntervals.end()) {
                 start = intervals[i][0];
                 end = intervals[i][1];
+                if (mapIntervals[start] >= end) continue;
+                mapIntervals[start] = end;
             } else {
-                // overlap
-                end = max(end, intervals[i][1]);
+                itr = mapIntervals.upper_bound(intervals[i][0]);
+                start = intervals[i][0];
+                end = intervals[i][1];
+                if (itr != mapIntervals.begin()) {
+                    int prevStart = prev(itr)->first;
+                    int prevEnd = prev(itr)->second;
+                    if (prevEnd >= start) {
+                        start = prevStart;
+                        end = max(end, prevEnd);
+                    }
+                }
+                mapIntervals[start] = end;
             }
+            mergeOverlapped(mapIntervals, start, end);
         }
-        result.push_back({start, end});
+
+        auto itr = mapIntervals.begin();
+        while (itr != mapIntervals.end()) {
+            result.push_back({itr->first, itr->second});
+            itr = next(itr);
+        }
         return result;
     }
 };
