71d346eb73d1406c73679dcd1b2dcecad1ddc47b
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2023-10-02 18:12:27 -0700

    38. Count and Say in C++ The LC Premium solution is more elegant and eliminates overheads of calling a function in my first solution.

--- problems/count-and-say/Solution.cpp
+++ problems/count-and-say/Solution.cpp
@@ -7,32 +7,15 @@ public:
     string countAndSay(int n) {
         string result = "1";
         for (int i = 2; i <= n; i++) {
-            int lastSeen = 0;
-            int count = 0;
             string nextStr;
-            for (int j = 0; j < result.size(); j++) {
-                parseString(result, j, to_string(result[j] - '0'), lastSeen, nextStr, count);
+            for (int j = 0, k = 0; k < result.size(); j = k) {
+                while (k < result.size() && result[k] == result[k + 1]) k++;
+                k++;
+                nextStr += to_string(k - j);
+                nextStr += result[k - 1];
             }
-            nextStr += to_string(count);
-            nextStr += result[result.size() - 1];
             result = nextStr;
         }
         return result;
-    }
-private:
-    void parseString(string result, int j, string digit, int &lastSeen, string &nextStr, int &count) {
-        if (result[j] == digit[0]) {
-            if (lastSeen != int(result[j] - '0')) {
-                if (lastSeen != 0) {
-                    nextStr += to_string(count);
-                    nextStr += result[j - 1];
-                }
-                lastSeen = int(result[j] - '0');
-                count = 1;
-            }
-            else {
-                count++;
-            }
-        }
     }
 };
