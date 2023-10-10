91252b6c2615f7f23367dc8df40ff549e1360fc6
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2023-10-10 15:12:48 -0700

    55. Jump Game in C++ Removed unnecessary check.

--- problems/jump-game/Solution2.cpp
+++ problems/jump-game/Solution2.cpp
@@ -7,7 +7,6 @@ class Solution2 {
 public:
     // tracing forward
     bool canJump(vector<int>& nums) {
-        if (nums.size() == 1) return true;
         for (int i = 0, idx = 0; i < nums.size() - 1; i++) {
             if (i + nums[i] > idx) idx = i + nums[i];
             if (idx == i) return false;
