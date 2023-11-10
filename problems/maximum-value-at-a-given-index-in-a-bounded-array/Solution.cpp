879276355ccd1a476e0fbcf5c63cd18e58f7adce
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2023-11-09 17:40:56 -0800

    Use binary search to find maximum value over the range of array.

--- problems/maximum-value-at-a-given-index-in-a-bounded-array/Solution.cpp
+++ problems/maximum-value-at-a-given-index-in-a-bounded-array/Solution.cpp
@@ -5,37 +5,54 @@ using namespace std;
 
 class Solution {
 public:
+    // In the solution, it uses 1 to maxSum for l and r which obviates the 2 if statements after while loop.
+    // I use bounds of array index (0 to n - 1 for l and r) to get the maximum sum.
+    // If maximum sum is < maxSum after exiting the loop, we just need to add the quotient to the value.
     int maxValue(int n, int index, int maxSum) {
-        int maximized = 1;
-        int l_max_height = index - 0, r_max_height = n - index;
-        int l = 0, m = 0, r = 0, l_height = 0, r_height = 0, l_sum = 0, r_sum = 0, sum = l_sum + r_sum;
-        while (n + l_sum + r_sum < maxSum) {
-            l_sum = (l + m - 1) * l_height / 2.0;
-            r_sum = (r + m) * r_height / 2.0;
-            if (n + l_sum + r_sum >= maxSum) {
-                if (n + l_sum + r_sum > maxSum) m--;
-                break;
-            }
-            if (l_height + r_height == n) {
-                m += floor((maxSum - n - l_sum - r_sum + 0.0) / n);
-                break;
-            }
-            m++;
-            if (m > 1 && l_height < l_max_height) {
-                l_height++;
-                l = 1;
+        int l = 0, r = n - 1;
+        long sum = 0;
+
+        while (l < r) {
+            int mid = ceil((l + r) / 2.0);
+            sum = getSum(index, mid + 1, n);
+            if (sum <= maxSum) {
+                l = mid;
             }
             else {
-                l++;
-            }
-            if (r_height < r_max_height) {
-                r_height++;
-                r = 1;
-            }
-            else {
-                r++;
+                r = mid - 1;
             }
         }
-        return maximized + m;
+        // sum is 0 when array size is 1.
+        // l is the index, not value.
+        if (sum != 0) l++;
+        if (sum < maxSum) {
+            l += (maxSum - sum) / n;
+        }
+        return l;
+    }
+private:
+    double getSum(int index, int value, int n) {
+        double sum = 0;
+        int l = 1, r = 1;
+        // right edge value
+        if (value - (n - index - 1) > 1) {
+            r = value - (n - index - 1);
+        }
+        // left edge value
+        if (value - index > 1) {
+            l = value - index;
+        }
+        // sum right of index
+        sum += (value + r) * ((value - r + 1) / 2.0);
+        if (r == 1) {
+            sum += n - index - value;
+        }
+        // sum left of index
+        sum += (l + value) * ((value - l + 1) / 2.0);
+        if (l == 1) {
+            sum += index - value + 1;
+        }
+        sum -= value;
+        return sum;
     }
 };
