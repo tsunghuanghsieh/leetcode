3a8bf8fc38e4a567419e0ea0da1fc69c832caf4e
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2023-11-27 17:02:10 -0800

    808. Soup Servings Changed member function to nested function. No significant change in the execution time. The if statement, n > 4800, reduced the execution time to less than 50ms.

--- problems/soup-servings/Solution.cpp
+++ problems/soup-servings/Solution.cpp
@@ -15,39 +15,64 @@ public:
     // With that, we will do it iteratively with small n until the probability becomes > 0.99999.
     double soupServings(int n) {
         double probability = 0.0;
+        vector<pair<int, int>> servings = {{100, 0}, {75, 25}, {50, 50}, {25, 75}};
+        unordered_map<int, unordered_map<int, double>> seen = {};
+        function<double(int, int)> getProbability = [&](int a, int b) -> double {
+            int emptyA, emptyB;
+            double this_probability = 0;
+            for (int i = 0; i < 4; i++) {
+                if (seen.find(a) != seen.end() && seen[a].find(b) != seen[a].end()) {
+                    this_probability += 0.25 * seen[a][b];
+                    continue;
+                }
+                int remainingA = a - servings[i].first;
+                int remainingB = b - servings[i].second;
+                emptyA = remainingA <= 0;
+                emptyB = remainingB <= 0;
+                if (emptyA && emptyB) {
+                    this_probability += 0.5 * 0.25;
+                } else if (emptyA) {
+                    this_probability += 1 * 0.25;
+                } else if (emptyB) {
+                    // do nothing
+                    this_probability += 0 * 0.25;
+                } else {
+                    this_probability += 0.25 * getProbability(remainingA, remainingB);
+                }
+            }
+            seen[a][b] = this_probability;
+            return this_probability;
+        };
+        //
+        // This runs faster on leetcode
+        //
+        // function<double(int, int)> getProbability = [&](int a, int b) -> double {
+        //     double this_probability = 0;
+        //     if (seen.find(a) != seen.end() && seen[a].find(b) != seen[a].end()) {
+        //         return seen[a][b];
+        //     }
+        //     if (a <= 0 && b <= 0) {
+        //         this_probability = 0.5;
+        //     } else if (a <= 0) {
+        //         this_probability = 1;
+        //     } else if (b <= 0) {
+        //         // do nothing
+        //         this_probability = 0;
+        //     } else {
+        //         this_probability = 0.25 * (getProbability(a - 100, b) + getProbability(a - 75, b - 25) +
+        //                                    getProbability(a - 50, b - 50) + getProbability(a - 25, b - 75));
+        //     }
+        //     seen[a][b] = this_probability;
+        //     return this_probability;
+        // };
+
+        // empirical data shows probability is 1.0 when n > 4800.
+        // without this, the execution will take ~500ms longer.
+        if (n > 4800) return 1.0;
         for (int i = 0; i < n + 25; i += 25) {
             probability = getProbability(i, i);
             if (probability > 1 - 1e-5) return 1.0;
         }
-        return probability;
-    }
-private:
-    vector<pair<int, int>> servings = {{100, 0}, {75, 25}, {50, 50}, {25, 75}};
-    unordered_map<int, unordered_map<int, double>> seen = {};
-    double getProbability(int a, int b) {
-        int emptyA, emptyB;
-        double probability = 0;
-        for (int i = 0; i < 4; i++) {
-            if (seen.find(a) != seen.end() && seen[a].find(b) != seen[a].end()) {
-                probability += 0.25 * seen[a][b];
-                continue;
-            }
-            int remainingA = a - servings[i].first;
-            int remainingB = b - servings[i].second;
-            emptyA = remainingA <= 0;
-            emptyB = remainingB <= 0;
-            if (emptyA && emptyB) {
-                probability += 0.5 * 0.25;
-            } else if (emptyA) {
-                probability += 1 * 0.25;
-            } else if (emptyB) {
-                // do nothing
-                probability += 0 * 0.25;
-            } else {
-                probability += 0.25 * getProbability(remainingA, remainingB);
-            }
-        }
-        seen[a][b] = probability;
         return probability;
     }
 };
