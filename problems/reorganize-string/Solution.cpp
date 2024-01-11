cbc40e83a408db27a2445f58bfffe56409bd3ae5
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-01-10 17:07:34 -0800

    Use vector instead of map, since the size, 26, is known and relatively small.

--- problems/reorganize-string/Solution.cpp
+++ problems/reorganize-string/Solution.cpp
@@ -10,32 +10,31 @@ using namespace std;
 class Solution {
 public:
     string reorganizeString(string s) {
-        char maxChar = 0;
-        map<char, int> freq;
+        int maxChar = 0;
+        vector<int> freq(26, 0);
         priority_queue<vector<int>> pq;
         stringstream ss;
 
         for (int i = 0; i < s.size(); i++) {
-            freq[s[i]]++;
-            maxChar = (maxChar == 0 || freq[s[i]] > freq[maxChar]) ? s[i] : maxChar;
+            freq[s[i] - 'a']++;
+            maxChar = (freq[s[i] - 'a'] > freq[maxChar]) ? s[i] - 'a' : maxChar;
         }
         if (freq[maxChar] - 1 > (s.size() - 1) / 2) {
             return "";
         }
-        for (pair<char, int> p : freq) {
-            pq.push({p.second, p.first - 'a'});
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) pq.push({freq[i], 'a' + i});
         }
-        cout << freq.size() << endl;
         while (!pq.empty()) {
             vector<int> chFirst = pq.top();
             pq.pop();
             chFirst[0]--;
-            ss << char(chFirst[1] + 'a');
+            ss << char(chFirst[1]);
             if (pq.empty()) break;
             vector<int> chSecond = pq.top();
             pq.pop();
             chSecond[0]--;
-            ss << char(chSecond[1] + 'a');
+            ss << char(chSecond[1]);
             if (chFirst[0] > 0) pq.push(chFirst);
             if (chSecond[0] > 0) pq.push(chSecond);
         }
