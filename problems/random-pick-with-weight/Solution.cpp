bbadc72b606fce0f296d61e6ba52a1c642f8c373
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-02-17 07:37:33 -0800

    Use vector<int> instead map<int, int> for performance.

--- problems/random-pick-with-weight/Solution.cpp
+++ problems/random-pick-with-weight/Solution.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
-#include <map>
 #include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-    Solution(vector<int>& w) {
+    Solution(vector<int>& w) : dist(w.size(), 0) {
         int size = w.size();
         for (int i = 0; i < size; i++) {
             sum += w[i];
-            dist[sum - 1] = i;
+            dist[i] = sum - 1;
         }
     }
 
     int pickIndex() {
-        return dist.lower_bound(rand() % sum)->second;
+        return lower_bound(dist.begin(), dist.end(), rand() % sum) - dist.begin();
     }
 private:
-    map<int, int> dist;
+    vector<int> dist;
     int sum = 0;
 };
 
