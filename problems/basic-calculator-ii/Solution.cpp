ee346faef37d4dac5e93b58365f31d11f16b81ec
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-02-16 17:23:30 -0800

    227. Basic Calculator II https://leetcode.com/problems/basic-calculator-ii Refactored based on LC Editorial.

--- problems/basic-calculator-ii/Solution.cpp
+++ problems/basic-calculator-ii/Solution.cpp
@@ -1,44 +1,30 @@
 #include <iostream>
-#include <queue>
-#include <regex>
-#include <string>
-#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
     int calculate(string s) {
-        int start = 0;
-        regex patternOps("[+|\\-|\\\\*|/]");
-        sregex_token_iterator itr(s.begin(), s.end(), patternOps);
-        queue<char> q_operators;
-        queue<int> q_operands;
-
-        for (; itr != sregex_token_iterator(); itr++) {
-            int idx = s.find(*itr, start);
-            q_operands.push(stoi(s.substr(start, idx - start)));
-            q_operators.push(*(*itr).str().c_str());
-            start = idx + 1;
-        }
-        q_operands.push(stoi(s.substr(start)));
-        char optr = 0;
-        int prev = q_operands.front(), oprd = 0, total = 0;
-        q_operands.pop();
-        while (q_operators.size() > 0) {
-            optr = q_operators.front();
-            q_operators.pop();
-            oprd = q_operands.front();
-            q_operands.pop();
-            if (optr == '+' || optr == '-') {
-                total += prev;
-                prev = (optr == '-') ? -oprd : oprd;
+        int total = 0, prev = 0, curr = 0;
+        char ch = 0, optr = '+';
+        for (int i = 0; i < s.size(); i++) {
+            ch = s[i];
+            if (isdigit(ch)) {
+                curr = curr * 10 + (ch - '0');
             }
-            else if (optr == '*') {
-                prev *= oprd;
-            }
-            else {
-                prev /= oprd;
+            if (ispunct(ch) || i + 1 == s.size()) {
+                if (optr == '+' || optr == '-') {
+                    total += prev;
+                    prev = (optr == '-') ? -curr : curr;
+                }
+                else if (optr == '*') {
+                    prev *= curr;
+                }
+                else if (optr == '/') {
+                    prev /= curr;
+                }
+                optr = ch;
+                curr = 0;
             }
         }
         total += prev;
