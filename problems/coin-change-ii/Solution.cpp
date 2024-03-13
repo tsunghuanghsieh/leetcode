b2eb9274bed8bb3c981d259ccd290d06a3262a51
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-03-13 05:57:22 -0700

    518. Coin Change II  Bottom-Up approach Initial commit of bottom-up approach.

--- problems/coin-change-ii/Solution.cpp
+++ problems/coin-change-ii/Solution.cpp
@@ -9,21 +9,19 @@ public:
     // of computations of previously discarded subtrees and tried to manually maintain the unique permutations
     // of coins. It failed LTE.
     // Also the amount was in the outer loop, coin in the inner loop.
-    vector<int> lcoins;
-    vector<vector<int>> res;
+    // For Top-Down approach, it doesn't compute all numbers in amount, only those divisible by combinations
+    // of coins. Its best exec time is faster the Bottom-up approach on LC.
     int change(int amount, vector<int>& coins) {
-        if (amount == 0) return 1;   // special case
-        lcoins = coins;
-        res.assign(lcoins.size(), vector<int>(amount + 1, -1));
-        sort(lcoins.begin(), lcoins.end());   // sorted coins will allow pruning
-        return findChange(amount, 0);
-    }
-private:
-    int findChange(int amount, int idx) {
-        if (idx == lcoins.size() || amount < lcoins[idx]) return 0;   // prune when amount < lcoins[idx]
-        if (amount == lcoins[idx]) return 1;
-        if (res[idx][amount] != -1) return res[idx][amount];
-        res[idx][amount] = findChange(amount - lcoins[idx], idx) + findChange(amount, idx + 1);
-        return res[idx][amount];
+        // Bottom-up approach
+        // Runtime is O(coins.size() * amount)
+        vector<vector<int>> res(coins.size() + 1, vector<int>(amount + 1, 0));
+        for (int i = 0; i <= coins.size(); i++) res[i][0] = 1;
+        for (int i = 1; i <= coins.size(); i++) {
+            for (int j = 0; j <= amount; j++) {
+                if (coins[i - 1] > j) res[i][j] = res[i - 1][j];
+                else res[i][j] = res[i][j - coins[i - 1]] + res[i - 1][j];
+            }
+        }
+        return res[coins.size()][amount];
     }
 };
