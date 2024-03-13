8dd212a0a22de1dfcefba07f6345913597c721f7
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-03-13 12:52:15 -0700

    518. Coin Change II  Bottom-Up approach Space optimization and additional comment.

--- problems/coin-change-ii/Solution.cpp
+++ problems/coin-change-ii/Solution.cpp
@@ -10,18 +10,21 @@ public:
     // of coins. It failed LTE.
     // Also the amount was in the outer loop, coin in the inner loop.
     // For Top-Down approach, it doesn't compute all numbers in amount, only those divisible by combinations
-    // of coins. Its best exec time is faster the Bottom-up approach on LC.
+    // of coins. Its best exec time (0 ms) is faster the Bottom-up approach on LC wo space optimization (7 ms).
+    // Bottom-up approach with space optimization runs at 0 ms more frequently than Top-down approach.
     int change(int amount, vector<int>& coins) {
         // Bottom-up approach
         // Runtime is O(coins.size() * amount)
-        vector<vector<int>> res(coins.size() + 1, vector<int>(amount + 1, 0));
-        for (int i = 0; i <= coins.size(); i++) res[i][0] = 1;
+
+        // To compute values of current row (coins[idx]), it needs res[i][j - coins[idx]] and res[i - 1][j].
+        // We can add the values and store the result in the current row, hence reducing 2D array into 1D array.
+        vector<int> res(amount + 1, 0);
+        res[0] = 1;
         for (int i = 1; i <= coins.size(); i++) {
-            for (int j = 0; j <= amount; j++) {
-                if (coins[i - 1] > j) res[i][j] = res[i - 1][j];
-                else res[i][j] = res[i][j - coins[i - 1]] + res[i - 1][j];
+            for (int j = coins[i - 1]; j <= amount; j++) {
+                res[j] = res[j - coins[i - 1]] + res[j];
             }
         }
-        return res[coins.size()][amount];
+        return res[amount];
     }
 };
