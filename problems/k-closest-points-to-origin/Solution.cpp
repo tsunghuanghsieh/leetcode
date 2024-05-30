ab86b03f8263b0e01b50edf5d26af83a3af92884
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-05-30 00:42:01 -0700

    973. K Closest Points to Origin https://leetcode.com/problems/k-closest-points-to-origin/ Optimization.

--- problems/k-closest-points-to-origin/Solution.cpp
+++ problems/k-closest-points-to-origin/Solution.cpp
@@ -8,14 +8,21 @@ class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         vector<vector<int>> res;
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> q;
+        priority_queue<vector<int>> q;   // max heap
 
         for (int i = 0; i < points.size(); i++) {
-            q.push({points[i][0] * points[i][0] + points[i][1] * points[i][1], i});
+            int dist = points[i][0] * points[i][0] + points[i][1] * points[i][1];
+            // cap size at k
+            // runtime O(nlogk) and space O(k)
+            // instead of O(nlogn) and O(n)
+            if (q.size() == k) {
+                if (dist >= q.top()[0]) continue;
+                q.pop();
+            }
+            q.push({dist, i});
         }
         for (int i = 0; i < k; i++) {
-            vector<int> item = q.top();
-            res.push_back(points[item[1]]);
+            res.push_back(points[q.top()[1]]);
             q.pop();
         }
         return res;
