b7807fa6f966024c35ca9064d56d25880fd185e8
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-06-05 10:53:11 -0700

    827. Making A Large Island https://leetcode.com/problems/making-a-large-island/ Updated grid in place. Passed LTE test.

--- problems/making-a-large-island/Solution.cpp
+++ problems/making-a-large-island/Solution.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <queue>
 #include <set>
 #include <vector>
 
@@ -7,37 +6,39 @@ using namespace std;
 
 class Solution {
 public:
+    // Update 1s in grid in place with island identifier using dfs.
+    // Tried using adjacents set to reduce the number of 0s to compare.
+    // Empirical data shows that time for set inserts, O(NlogN), adds up when the number is in 10s of 1000.
     int largestIsland(vector<vector<int>>& grid) {
         int n = grid.size();
-        vector<set<pair<int, int>>> islands;
-        set<pair<int, int>> seen, adjacents;
-        queue<pair<int, int>> coors;
-
-        printGrid(grid);
-        coors.push({0, 0});
-        // bfs to identify all islands in the grid, O(n^2)
-        while (coors.size() > 0) {
-            int row = coors.front().first, col = coors.front().second;
-            coors.pop();
-
-            // Comparing each element against all islands get really slow when the number is big, say 10s of 1k.
-            // Similarly, insert to islands and adjacents sets takes longer.
-            processGrid(n, row, col, grid[row][col], islands, adjacents);
-            if (isInBound(goDirection(Direction::EAST, row, col), n) &&
-                seen.count(goDirection(Direction::EAST, row, col)) == 0) {
-                coors.push(goDirection(Direction::EAST, row, col));
-                seen.insert(goDirection(Direction::EAST, row, col));
-            }
-            if (isInBound(goDirection(Direction::SOUTH, row, col), n) &&
-                seen.count(goDirection(Direction::SOUTH, row, col)) == 0) {
-                coors.push(goDirection(Direction::SOUTH, row, col));
-                seen.insert(goDirection(Direction::SOUTH, row, col));
+        int island_id = 2, largest = 0;
+        vector<int> islands(n * n);
+        if (n < 25) printGrid(grid);
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                if (grid[row][col] == 1) {
+                    // dfs of current island and set value to index
+                    islands[island_id - 2] = dfs(grid, island_id, row, col);
+                    island_id++;
+                }
             }
         }
-        if (islands.size() == 0) return 1;
-        if (islands.size() == 1) return min(int(islands[0].size() + 1), n * n);
-        // Comparing all adjacents against all islands takes a long time when the number is big, say 10s of 1k.
-        return findLargest(n, adjacents, islands);
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                int count = 1;
+                set<int> dirs;
+                if (grid[row][col] != 0) continue;
+                for (int d = 0; d < 4; d++) {
+                    auto coor = goDirection(Direction(d), row, col);
+                    if (!isInBound(coor, grid.size())) continue;
+                    if (grid[coor.first][coor.second] != 0) dirs.insert(grid[coor.first][coor.second] - 2);
+                }
+                for (int i : dirs) count += islands[i];
+                largest = max(largest, count);
+            }
+        }
+        // largest is 0 when the grid has no 0s
+        return (largest) ? largest : n * n;
     }
 
     // helper function to help visualize grid on console
@@ -53,24 +54,15 @@ private:
     enum Direction { WEST = 0, NORTH = 1, EAST = 2, SOUTH = 3};
     const vector<pair<int, int>> movements = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};   // west, north, east, south
 
-    int findLargest(int size, set<pair<int, int>>& adjacents, vector<set<pair<int, int>>>& islands) {
-        int largest = 0;
-        for (auto p : adjacents) {
-            int total = 1;
-            vector<int> indices(islands.size(), 0);
-            for (int d = 0; d < 4; d++) {
-                auto coor = goDirection(Direction(d), p.first, p.second);
-                if (!isInBound(coor, size)) continue;
-                for (int i = 0; i < islands.size(); i++) {
-                    if (islands[i].count(coor) > 0 && indices[i] == 0) {
-                        indices[i] = 1;
-                        total += islands[i].size();
-                    }
-                }
-            }
-            largest = max(largest, total);
+    int dfs(vector<vector<int>>& grid, const int island_id, int row, int col) {
+        int count = 1;
+        grid[row][col] = island_id;
+        for (int d = 0; d < 4; d++) {
+            auto coor = goDirection(Direction(d), row, col);
+            if (!isInBound(coor, grid.size()) || grid[coor.first][coor.second] != 1) continue;
+            count += dfs(grid, island_id, coor.first, coor.second);
         }
-        return largest;
+        return count;
     }
 
     inline pair<int, int> goDirection(const Direction dir, const int &row, const int &col) {
@@ -80,67 +72,5 @@ private:
     inline bool isInBound(pair<int, int> coor, int dimension) {
         return (coor.first < 0 || coor.first == dimension ||
                 coor.second < 0 || coor.second == dimension) ? false : true;
-    }
-
-    void processGrid(int size, int row, int col, int val, vector<set<pair<int, int>>>& islands, set<pair<int, int>>& adjacents) {
-        int prevIsland = -1;
-        for (int d = 0; d < 2; d++) { // just need to check west and north. we haven't sesen east and south yet.
-            auto coor = goDirection(Direction(d), row, col);
-            if (!isInBound(coor, size)) continue;
-            for (int i = 0, count = 0; i < islands.size(); i++) {
-                if (islands[i].count(coor) > 0) {
-                    count++;
-                    if (val == 1) {
-                        if (prevIsland < 0) {
-                            prevIsland = i;
-                            islands[prevIsland].insert(make_pair(row, col));
-                        }
-                        else {
-                            if (prevIsland != i) {
-                                // different island
-                                mergeIslands(islands[prevIsland], islands[i]);
-                                islands.erase(islands.begin() + i);
-                            }
-                            return;
-                        }
-                        break;
-                    }
-                    else {
-                        adjacents.insert({row, col});
-                    }
-                }
-                else {
-                    if (i + 1 == islands.size() && val == 1 && count == 0) {
-                        adjacents.insert(coor);
-                    }
-                }
-            }
-        }
-        if (val == 1 && prevIsland == -1) {
-            set<pair<int, int>> island({make_pair(row, col)});
-            islands.push_back(island);
-        }
-    }
-
-    // Helper function to merge 2 islands into 1 and reset fromIsland
-    void mergeIslands(set<pair<int, int>>& toIsland, set<pair<int, int>>& fromIsland) {
-        if (toIsland.size() < fromIsland.size()) toIsland.swap(fromIsland);
-        toIsland.insert(fromIsland.begin(), fromIsland.end());
-        set<pair<int, int>> empty;
-        fromIsland.swap(empty);
-    }
-
-    // Helper function for validation
-    void listAdjacents(const set<pair<int, int>>& adjacents) {
-        for (auto p : adjacents) {
-            cout << p.first << " " << p.second << endl;
-        }
-    }
-
-    // Helper function for validation
-    void listIslands(vector<set<pair<int, int>>>& islands) {
-        for (int i = 0; i < islands.size(); i++) {
-            cout << "i " << i << " size " << islands[i].size() << endl;
-        }
     }
 };
