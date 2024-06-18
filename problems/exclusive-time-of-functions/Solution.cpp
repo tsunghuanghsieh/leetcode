71e1a9c3f69e7427c7cfe15513bf56860c764e68
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-06-18 02:24:36 -0700

    636. Exclusive Time of Functions https://leetcode.com/problems/exclusive-time-of-functions/ Refactored for optimization.

--- problems/exclusive-time-of-functions/Solution.cpp
+++ problems/exclusive-time-of-functions/Solution.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <regex>
 #include <stack>
 #include <vector>
 
@@ -7,32 +6,41 @@ using namespace std;
 
 class Solution {
 public:
+    // The initial approach was to keep track of timestamps in a stack, and a stack of stacks
+    // for the running sums of durations for other processes at each start of a process, so that
+    // when the process ends, it will subtract the running sum of durations for all other process
+    // between the current process' start and end. The current running sum will then be added to
+    // the top of the stack. This is not efficient when the stack runs deep.
+    //
+    // A better approach is to calculate the duration for the current process at each timestamp in the log
+    // as logs are processed. It just needs the last and the current timestamp to calculate duration.
     vector<int> exclusiveTime(int n, vector<string>& logs) {
         vector<int> res(n);
-        stack<int> ptime;
-        stack<stack<int>> stkOthers;
-        regex patternLog("(\\d+):(\\w+):(\\d+)");
+        stack<int> pid;
+        int prev_t;
         for (auto log : logs) {
-            sregex_token_iterator itr(log.begin(), log.end(), patternLog, {1, 2, 3});
-            int id = stoi((*itr++).str());
-            string op = *itr++;
-            int t = stoi((*itr).str());
-            if (op == "start") {
-                ptime.push(t);
-                stkOthers.push(stack<int>());
+            int id, t;
+            char op[6];
+            // use sscanf to read int and char array
+            sscanf(log.c_str(), "%d:%[a-z]:%d", &id, op, &t);
+            if (op[0] == 's') {
+                if (!pid.empty()) {
+                    // - 1 to offset the + 1 while handling the end event.
+                    res[pid.top()] += t - prev_t - 1;
+                }
+                prev_t = t;
+                pid.push(id);
             }
             else {
-                int duration = t - ptime.top() + 1;
-                stack<int> curr = stkOthers.top();
-                stkOthers.pop();
-                int curr_total = 0;
-                while (curr.size() > 0) {
-                    curr_total += curr.top();
-                    curr.pop();
-                }
-                if (stkOthers.size() > 0) stkOthers.top().push(duration);
-                res[id] += duration - curr_total;
-                ptime.pop();
+                // This would pass test case 121 where a process not currently being executed ends and
+                // pops another process currently being executed off the stack.
+                res[pid.top()] += t - prev_t + 1;
+
+                // This follows the understanding that only currently executed process can pop itself off the stack
+                // and fails test case 121.
+                // res[id] += t - prev_t + 1;
+                prev_t = t;
+                pid.pop();
             }
         }
         return res;
