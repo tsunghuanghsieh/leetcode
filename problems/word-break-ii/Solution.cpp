272cef56878a4c4cd110f86c2f793bd020dc2111
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-07-08 12:00:58 -0700

    140. Word Break II https://leetcode.com/problems/word-break-ii/ Added comment.

--- problems/word-break-ii/Solution.cpp
+++ problems/word-break-ii/Solution.cpp
@@ -7,6 +7,31 @@
 using namespace std;
 
 class Solution {
+// In all LC editorial solutions, they all take a substring, length 1 to entire string, of s
+// and compare it to wordDict. It will be inefficient when substring is shorter or longer than all words
+// in wordDict.
+//
+// Instead, my approach is to take words in wordDict and compare them to s. If a match is found,
+// repeat the comparisons at the new starting index until the end of s. Otherwise, s cannot be constructed
+// using words in wordDict.
+//
+// After that, reconstruct the sentences from visited using dfs. The sentence is valid only when it ends on
+// position s.size().
+//
+// all possible permutations
+// 00 01 02 03 04 05 06 07 08 09 10 11 12 13
+//  c  a  t  s  a  n  d  d  o  g  s  a  n  d
+//  3        7          10       14           cat sand dog sand
+//  3        7          11          14        cat sand dogs and
+//  4           7       10       14           cats and dog sand
+//  4           7       11          14        cats and dogs and
+//
+// recorded in visited
+// 00 01 02 03 04 05 06 07 08 09 10 11 12 13
+//  c  a  t  s  a  n  d  d  o  g  s  a  n  d
+//  3        7          10       14
+//  4           7       11          14
+
 public:
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         visited.resize(s.size());
