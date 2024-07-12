0c54219c00a9bbfd8d0ffdb58a630874fd8e332e
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-07-12 13:59:43 -0700

    126. Word Ladder II https://leetcode.com/problems/word-ladder-ii/ First commit. Failed LC tc 32 (test5.txt), 14^8 permutations, with TLE.

---
+++ problems/word-ladder-ii/Solution.cpp
@@ -0,0 +1,77 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+        vector<vector<string>> results;
+        queue<string> currNeighbors({beginWord});
+        set<string> known({beginWord}), uniqueCurrNeighbors;
+        int currNeighborsCount = currNeighbors.size();
+
+        // BFS to find endWord
+        // iteratively finding adjacent neighbors until endWord is found or wordList is exhausted
+        while (currNeighborsCount != 0) {
+            string currWord = currNeighbors.front();
+            currNeighbors.pop();
+            currNeighborsCount--;
+
+            vector<string> newNeighbors = findNeighbors(currWord, wordList, known);
+
+            directedGraph[currWord] = newNeighbors;
+            for (string neighbor : newNeighbors) {
+                uniqueCurrNeighbors.insert(neighbor);
+            }
+            if (currNeighborsCount == 0) {
+                if (uniqueCurrNeighbors.count(endWord)) {
+                    break;
+                }
+
+                for (auto itr = uniqueCurrNeighbors.begin(); itr != uniqueCurrNeighbors.end(); itr++) {
+                    currNeighbors.push(*itr);
+                    known.insert(*itr);
+                }
+                currNeighborsCount = currNeighbors.size();
+                uniqueCurrNeighbors.clear();
+            }
+        }
+
+        constructLadder(beginWord, endWord, vector<string>({beginWord}), results);
+        return results;
+    }
+private:
+    unordered_map<string, vector<string>> directedGraph;
+
+    void constructLadder(const string& beginWord, const string& endWord, vector<string> ladder, vector<vector<string>>& results) {
+        if (beginWord.compare(endWord) == 0) {
+            results.emplace_back(ladder);
+            return;
+        }
+        if (directedGraph.count(beginWord) == 0) return;
+        for (string s : directedGraph[beginWord]) {
+            ladder.emplace(ladder.begin(), s);
+            constructLadder(s, endWord, ladder, results);
+            ladder.erase(ladder.begin());
+        }
+    }
+
+    vector<string> findNeighbors(string currWord, vector<string>& wordList, set<string>& visited) {
+        vector<string> results;
+        for (auto itr = wordList.begin(); itr != wordList.end(); itr++) {
+            if (visited.count(*itr) > 0) continue;
+            int diff = 0;
+            for (int i = 0; i < currWord.size(); i++) {
+                if ((*itr)[i] != currWord[i]) diff++;
+                if (diff > 1) break;
+            }
+            if (diff == 1) results.emplace_back(*itr);
+        }
+        return results;
+    }
+};
