259ce7a3d15aeb0dcbe55a01371de5f2de240209
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-07-16 00:02:37 -0700

    126. Word Ladder II https://leetcode.com/problems/word-ladder-ii/ Optimization. Updated comparison algorithm to find neighbors.

--- problems/word-ladder-ii/Solution.cpp
+++ problems/word-ladder-ii/Solution.cpp
@@ -12,7 +12,7 @@ public:
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
         vector<vector<string>> results;
         queue<string> currNeighbors({beginWord});
-        unordered_set<string> known({beginWord}), uniqueCurrNeighbors;
+        unordered_set<string> wordListSet(wordList.begin(), wordList.end()), uniqueNewNeighbors;
         int currNeighborsCount = currNeighbors.size();
 
         // BFS to find endWord
@@ -22,23 +22,20 @@ public:
             currNeighbors.pop();
             currNeighborsCount--;
 
-            vector<string> newNeighbors = findNeighbors(currWord, wordList, known);
-
+            vector<string> newNeighbors = findNeighbors(currWord, wordListSet);
             for (string neighbor : newNeighbors) {
-                uniqueCurrNeighbors.insert(neighbor);
+                uniqueNewNeighbors.insert(neighbor);
                 inverseGraph[neighbor].emplace_back(currWord);
             }
             if (currNeighborsCount == 0) {
-                if (uniqueCurrNeighbors.count(endWord)) {
-                    break;
-                }
+                if (uniqueNewNeighbors.count(endWord)) break;
 
-                for (auto itr = uniqueCurrNeighbors.begin(); itr != uniqueCurrNeighbors.end(); itr++) {
+                for (auto itr = uniqueNewNeighbors.begin(); itr != uniqueNewNeighbors.end(); itr++) {
                     currNeighbors.push(*itr);
-                    known.insert(*itr);
+                    wordListSet.erase(*itr);
                 }
                 currNeighborsCount = currNeighbors.size();
-                uniqueCurrNeighbors.clear();
+                uniqueNewNeighbors.clear();
             }
         }
 
@@ -60,16 +57,21 @@ private:
         }
     }
 
-    vector<string> findNeighbors(string currWord, vector<string>& wordList, unordered_set<string>& visited) {
+    // Instead of using wordList in vector<string> and keeping a separate set<string> for visited,
+    // copy it to a unorder_set<string> for faster lookup and removal of visited keys, which enables
+    // key finding in unordered_set, according to LC Editorial O(25*k*k+1) or O(k^2) where k is string.size(),
+    // instead of explicitly doing string comparison O(nk) where n is wordList.size() and k is string.size().
+    vector<string> findNeighbors(string currWord, unordered_set<string>& wordListSet) {
         vector<string> results;
-        for (auto itr = wordList.begin(); itr != wordList.end(); itr++) {
-            if (visited.count(*itr) > 0) continue;
-            int diff = 0;
-            for (int i = 0; i < currWord.size(); i++) {
-                if ((*itr)[i] != currWord[i]) diff++;
-                if (diff > 1) break;
+        for (int i = 0; i < currWord.size(); i++) {
+            string ori(currWord);
+            char oriChar = currWord[i];
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                currWord[i] = ch;
+                if (ch == oriChar || wordListSet.count(currWord) == 0) continue;
+                results.emplace_back(currWord);
             }
-            if (diff == 1) results.emplace_back(*itr);
+            currWord[i] = oriChar;
         }
         return results;
     }
