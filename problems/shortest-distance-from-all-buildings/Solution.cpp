b8782e2772e8e138d8f90001609ff3b6d47ac09f
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-07-25 19:40:55 -0700

    317. Shortest Distance from All Buildings https://leetcode.com/problems/shortest-distance-from-all-buildings/ Restored to unordered_set<int> since it passes all LC test cases.

--- problems/shortest-distance-from-all-buildings/Solution.cpp
+++ problems/shortest-distance-from-all-buildings/Solution.cpp
@@ -1,7 +1,7 @@
 #include <iomanip>
 #include <iostream>
 #include <queue>
-#include <set>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
@@ -23,40 +23,40 @@ public:
     // however it fails at LC test case 84 (test8.txt) with TLE. Running test case 84 individually passes.
     int shortestDistance(vector<vector<int>>& grid) {
         int shortest = INT_MAX, row_size = grid.size(), col_size = grid[0].size();
-        set<pair<int, int>> pos0s, pos1s;
+        unordered_set<int> pos0s, pos1s;
         vector<vector<int>> dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};   // up, right, down, left
 
         for (int i = 0; i < row_size; i++) {
             for (int j = 0; j < col_size; j++) {
                 if (grid[i][j] == 0) {
-                    pos0s.insert({i, j});
+                    pos0s.insert(i * col_size + j);
                     continue;
                 }
-                if (grid[i][j] == 1) pos1s.insert({i, j});
+                if (grid[i][j] == 1) pos1s.insert(i * col_size + j);
                 grid[i][j] = INT_MAX;
             }
         }
         if (pos0s.size() == 0) return -1;
-        for (auto [r1, c1] : pos1s) {
+        for (int pos : pos1s) {
             // do bfs from each building to determine distance from all empty space in the grid
-            set<pair<int, int>> visited, seen1;
-            queue<pair<int, int>> q;
-            q.push({r1, c1});
-            seen1.insert({r1, c1});
+            unordered_set<int> visited, seen1;
+            queue<int> q;
+            q.push(pos);
+            seen1.insert(pos);
             int qsize = q.size(), step = 1;
             while (qsize > 0) {
-                int r = q.front().first, c = q.front().second;
+                int val = q.front(), r = val / col_size, c = val % col_size;
                 q.pop();
                 qsize--;
-                visited.insert({r, c});
+                visited.insert(val);
                 for (vector<int> dir : dirs) {
-                    int dr = r + dir[0], dc = c + dir[1];
-                    if (pos1s.count({dr, dc}) > 0) seen1.insert({dr, dc});
-                    if (visited.count({dr, dc}) > 0 || dr < 0 || dr >= row_size || dc < 0 || dc >= col_size) continue;
+                    int dr = r + dir[0], dc = c + dir[1], adj = dr * col_size + dc;
+                    if (pos1s.count(adj) > 0) seen1.insert(adj);
+                    if (visited.count(adj) > 0 || dr < 0 || dr >= row_size || dc < 0 || dc >= col_size) continue;
                     if (grid[dr][dc] == INT_MAX) continue;
                     grid[dr][dc] += step;
-                    q.push({dr, dc});
-                    visited.insert({dr, dc});
+                    q.push(adj);
+                    visited.insert(adj);
                 }
                 if (qsize == 0) {
                     qsize = q.size();
@@ -64,13 +64,13 @@ public:
                 }
             }
             if (pos1s.size() != seen1.size()) return -1;   // some buildings are unreachable
-            for (auto [r0, c0] : pos0s) {
-                if (visited.count({r0, c0}) > 0) continue;
-                grid[r0][c0] = INT_MAX;   // marking unreachable empty spaces
+            for (int pos : pos0s) {
+                if (visited.count(pos) > 0) continue;
+                grid[pos / col_size][pos % col_size] = INT_MAX;   // marking unreachable empty spaces
             }
         }
-        for (auto [r0, c0] : pos0s) {
-            shortest = min(shortest, grid[r0][c0]);
+        for (int pos : pos0s) {
+            shortest = min(shortest, grid[pos / col_size][pos % col_size]);
         }
         return (shortest != INT_MAX) ? shortest : -1;
     }
