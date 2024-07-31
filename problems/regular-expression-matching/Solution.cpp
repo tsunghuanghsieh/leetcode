ddfeb5ab15ab92fbcaacc3ef453c0adc2d32fa23
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-07-31 11:48:56 -0700

    10. Regular Expression Matching https://leetcode.com/problems/regular-expression-matching/ First commit of working code.

---
+++ problems/regular-expression-matching/Solution.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        return isMatch(s, 0, p, 0);
+    }
+private:
+    unordered_map<int, unordered_map<int, bool>> checked;
+    bool isMatch(string& s, int si, string& p, int pi) {
+        int ssize = s.size(), psize = p.size();
+        char lookAhead = 0;
+
+        if (checked[si].count(pi) > 0) return checked[si][pi];
+        if (si == ssize && pi == psize) return true;
+        if (si < ssize && pi == psize) return false;
+
+        if (p[pi] - 'a' >= 0 && p[pi] - 'a' < 26) {
+            if (pi + 1 < psize) lookAhead = p[pi + 1];
+            if (lookAhead == '*') {
+                if (p[pi] != s[si]) checked[si][pi] = isMatch(s, si, p, pi + 2);
+                else {
+                    if (si == ssize) checked[si][pi] = isMatch(s, si, p, pi + 2);
+                    else checked[si][pi] = isMatch(s, si, p, pi + 2) || isMatch(s, si + 1, p, pi);
+                }
+            }
+            else {
+                if (si == ssize || p[pi] != s[si]) checked[si][pi] = false;
+                else checked[si][pi] = isMatch(s, si + 1, p, pi + 1);
+            }
+        }
+        else {
+            // case: '.'
+            if (pi + 1 < psize) lookAhead = p[pi + 1];
+            if (lookAhead == '*') {
+                if (si == ssize) checked[si][pi] = isMatch(s, si, p, pi + 2);
+                else checked[si][pi] = isMatch(s, si, p, pi + 2) || isMatch(s, si + 1, p, pi);
+            }
+            else {
+                if (si == ssize) checked[si][pi] = false;
+                else checked[si][pi] = isMatch(s, si + 1, p, pi + 1);
+            }
+        }
+        return checked[si][pi];
+    }
+};
