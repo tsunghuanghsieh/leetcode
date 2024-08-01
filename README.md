f2062db0b7531bb10c5581e26bb02bc3c481027d
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-08-01 00:01:18 -0700

    938. Range Sum of BST https://leetcode.com/problems/range-sum-of-bst/ First commit of working code.

--- README.md
+++ README.md
@@ -147,4 +147,5 @@
 |   143 | [Shortest Distance from All Buildings](https://github.com/tsunghuanghsieh/leetcode/tree/master/problems/shortest-distance-from-all-buildings/)| Hard | C++ | Y |
 |   144 | [Product of Array Except Self](https://github.com/tsunghuanghsieh/leetcode/tree/master/problems/product-of-array-except-self/)| Medium | C++ ||
 |   145 | [Regular Expression Matching](https://github.com/tsunghuanghsieh/leetcode/tree/master/problems/regular-expression-matching/)| Hard | C++ ||
+|   146 | [Range Sum of BST](https://github.com/tsunghuanghsieh/leetcode/tree/master/problems/range-sum-of-bst/)| Easy | C++ ||
 
---
+++ problems/range-sum-of-bst/Solution.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        int sum = 0;
+
+        if (root == nullptr) return sum;
+        if (low <= root->val && high >= root->val) sum += root->val;
+        if (root->left && low < root->val) sum += rangeSumBST(root->left, low, high);
+        if (root->right && high > root->val) sum += rangeSumBST(root->right, low, high);
+        return sum;
+    }
+};
