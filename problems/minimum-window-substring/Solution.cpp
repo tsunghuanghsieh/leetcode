74d3ab32a8b356a2916e7a09cb5453e21b559e3a
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-08-18 02:00:22 -0700

    76. Minimum Window Substring https://leetcode.com/problems/minimum-window-substring/ Improvements. First working code passing all LC 268 test cases. Passed all previous TLE errors and additional TLE tc (test8.txt, LC tc 265, out of 268).

--- problems/minimum-window-substring/Solution.cpp
+++ problems/minimum-window-substring/Solution.cpp
@@ -8,67 +8,47 @@ using namespace std;
 
 class Solution {
 public:
-    // The approach is to identify letters in t and their frequency and filter s based on that to get
+    // Based on LC Editorial solutions, I can use sliding window to keep track of distinct letters in t
+    // encountered in s in their own queue, instead of vector<int> then brute forcing my way through in
+    // my initial approach. Once all letters in t are encountered in s, it can calculate the minimum
+    // sliding window for the first time. Subsequently another letter in t is encountered, the corresponding
+    // queue is updated and the minimum sliding window is recalculated.
+    // Array is used instead of vector in order to pass LC test case 265 (test8.txt) TLE.
+    //
+    // The initial approach is to identify letters in t and their frequency and filter s based on that to get
     // their positions in s. I failed to identify the pattern and used brute force to find the minimum
     // window substring. Even with memoization and pruning, it passes LC test case 224 (test6.txt), out
     // of 268 total. However, it is not able to pass LC test case 264 (test7.txt) TLE.
     string minWindow(string s, string t) {
-        if (t.size() > s.size()) return "";
-
-        int m = s.size(), n = t.size();
-        int first = m, last = 0;
-        int vs[52], vt[52];
-        unordered_map<char, vector<int>> matches;
-        vector<vector<int>> positions;
-        vector<int> counts;
-        res[0] = INT_MAX;
-        for (int i = 0; i < 52; i++) vt[i] = vs[i] = 0;
-        for (char c : t) {
-            vt[(c - 'a' < 0) ? c - 'A' : c - 'a' + 26]++;
-        }
+        int m = s.size(), n = t.size(), first = m, last = 0, distinct = 0;
+        int at[52];   // lightweight, compared to vector<int>
+        queue<int> matches[52];
+        unordered_set<char> matches_set;
+        for (int i = 0; i < 52; i++) at[i] = 0;   // required init
+        for (char ch : t) at[(ch - 'a' < 0) ? ch - 'A' : ch - 'a' + 26]++;
+        for (int i = 0; i < 52; i++) if (at[i] > 0) distinct++;   // count of distinct letters in t
         for (int i = 0; i < m; i++) {
-            if (vt[(s[i] - 'a' < 0) ? s[i] - 'A' : s[i] - 'a' + 26] == 0) continue;
-            vs[(s[i] - 'a' < 0) ? s[i] - 'A' : s[i] - 'a' + 26]++;
-            matches[s[i]].emplace_back(i);
-        }
-        for (int i = 0; i < 52; i++) {
-            if (vt[i] == 0) continue;
-            if (vt[i] > vs[i]) return "";
-            if (vt[i] == vs[i]) {
-                char ch = (i < 26) ? 'A' + i : 'a' + i - 26;
-                first = min(first, matches[ch][0]);
-                last = max(last, matches[ch][matches[ch].size() - 1]);
+            int idx = (s[i] - 'a' < 0) ? s[i] - 'A' : s[i] - 'a' + 26;
+            if (at[idx] > 0) {
+                matches[idx].push(i);
+                matches_set.insert(s[i]);   // count
+                if (matches[idx].size() > at[idx]) matches[idx].pop();   // pop the oldest
+                if (matches_set.size() == distinct) {
+                    int local_first = m, local_last = 0, count = 0;
+                    for (int j = 0; j < 52; j++) {
+                        if (at[j] == 0) continue;   // letters in s but not in t
+                        if (matches[j].size() < at[j]) break;   // letters in both s and t, but short
+                        count++;
+                        local_first = min(local_first, matches[j].front());
+                        local_last = max(local_last, matches[j].back());
+                    }
+                    if (count == matches_set.size() && abs(last - first) > local_last - local_first) {
+                        first = local_first;
+                        last = local_last;
+                    }
+                }
             }
-            else {
-                positions.emplace_back(matches[(i < 26) ? 'A' + i : 'a' + i - 26]);
-                counts.emplace_back(vt[i]);
-            }
         }
-
-        dfs(positions, 0, 0, counts, first, last);
-        return s.substr(res[1], res[0] + 1);
-    }
-private:
-    int res[3];
-    set<pair<int, int>> memo;
-    void dfs(vector<vector<int>>& positions, int r, int c, vector<int>& counts, int first, int last) {
-        if (res[0] < last - first) return;
-        if (memo.count(pair<int, int>(first, last)) > 0) return;
-        if (r >= positions.size()) {
-            memo.insert(pair<int, int>(first, last));
-            res[0] = last - first;
-            res[1] = first;
-            res[2] = last;
-            return;
-        }
-
-        if (c + counts[r] < positions[r].size()) {
-            dfs(positions, r, c + 1, counts, first, last);
-        }
-        first = min(first, positions[r][c]);
-        last = max(last, positions[r][c + counts[r] - 1]);
-        dfs(positions, r + 1, 0, counts, first, last);
-        memo.insert(pair<int, int>(first, last));
-        return;
+        return (first == m && last == 0) ? "" : s.substr(first, last - first + 1);
     }
 };
