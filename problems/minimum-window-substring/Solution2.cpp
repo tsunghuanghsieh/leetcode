a08e4af127a40238059a98e97bfc9460973bbe5a
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-08-21 00:08:48 -0700

    76. Minimum Window Substring https://leetcode.com/problems/minimum-window-substring/ Changed to array to reduce execution time on LC platform.

--- problems/minimum-window-substring/Solution2.cpp
+++ problems/minimum-window-substring/Solution2.cpp
@@ -13,24 +13,27 @@ public:
     // until some letter in t is missing. It then resumes incrementing r again. The process
     // repeats until end of s is reached.
     string minWindow(string s, string t) {
-        int m = s.size(), n = t.size();
-        unordered_map<char, int> um_t;
-        for (char ch : t) um_t[ch]++;
+        int m = s.size(), n = t.size(), distinct = 0;
+        int at[52], freq[52];   // lightweight, compared to vector<int>
+        for (int i = 0; i < 52; i++) at[i] = freq[i] = 0;   // required init
+        for (char ch : t) at[(ch - 'a' < 0) ? ch - 'A' : ch - 'a' + 26]++;
+        for (int i = 0; i < 52; i++) if (at[i] > 0) distinct++;   // count of distinct letters in t
 
-        unordered_map<char, int> freq;
         int min_l = 0, min_r = INT_MAX;
         for (int l = 0, r =  0, count = 0; r < m; r++) {
-            if (um_t.count(s[r])) {
-                freq[s[r]]++;
-                if (freq[s[r]] == um_t[s[r]]) count++;   // letter count in t met, increment count
-                // all letters in t met,
-                while (l <= r && count == um_t.size()) {
-                    if (um_t.count(s[l])) {
+            int idx_r = (s[r] - 'a' < 0) ? s[r] - 'A' : s[r] - 'a' + 26;
+            if (at[idx_r] > 0) {
+                freq[idx_r]++;
+                if (freq[idx_r] == at[idx_r]) count++;   // letter count in t met, increment count
+                // all letters in t met
+                while (l <= r && count == distinct) {
+                    int idx_l = (s[l] - 'a' < 0) ? s[l] - 'A' : s[l] - 'a' + 26;
+                    if (at[idx_l]) {
                         if (min_r - min_l > r - l) {
                             min_r = r;
                             min_l = l;
                         }
-                        if (--freq[s[l]] < um_t[s[l]]) count--;
+                        if (--freq[idx_l] < at[idx_l]) count--;
                     }
                     l++;
                 }
