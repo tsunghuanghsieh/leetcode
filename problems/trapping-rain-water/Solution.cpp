ebea3705135f5476f8b6a06a623f1657ef321d29
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-08-30 16:16:26 -0700

    42. Trapping Rain Water Added comment.

--- problems/trapping-rain-water/Solution.cpp
+++ problems/trapping-rain-water/Solution.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 class Solution {
 public:
+    // This implementation resembles LC soln 2 and has O(n) runtime and space complexity.
+    //
+    // The solution I came up with second time around is to use stack<int> to store
+    // max height and decreasing heights observed thereafter traversing left to right.
+    // When a taller height is then encountered, it will compare against the top of stack
+    // to determine water trapped, pop the top of stack off when it is taller and repeat
+    // until it is shorter than the top of stack or the stack is empty before pushing
+    // the taller height onto the stack. This approach resembles LC soln 3 and has O(n)
+    // runtime and space complexity like LC soln 2.
+    //
+    // LC soln 4 has O(n) runtime complexity and O(1) space complexity. It uses 2 pointers
+    // to keep track of left max and right max height respectively, traversing from both ends
+    // towards the middle (when left max is taller, traverse from right to middle; right max
+    // is taller, left to middle) and calculate water trapped along the way.
     int trap(vector<int>& height) {
         int units = 0;
         int size = height.size();
