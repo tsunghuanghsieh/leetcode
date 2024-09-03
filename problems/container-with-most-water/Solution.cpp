8ad86f842fe9ef5f19767a967e3aa91e1368fc60
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-09-03 00:16:36 -0700

    11. Container With Most Water https://leetcode.com/problems/container-with-most-water/ Added comment.

--- problems/container-with-most-water/Solution.cpp
+++ problems/container-with-most-water/Solution.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 class Solution {
 public:
+    // The idea is to start from the 2 ends and traverse inward. Since the amount of water is constrained
+    // by the lower end, we move the lower end inward and then determine the new lower end. This process
+    // repeats until the 2 ends meet. This is also LC Editorial soln 2.
     int maxArea(vector<int>& height) {
         int maxa = 0;
 
