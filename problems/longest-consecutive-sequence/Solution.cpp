0db474737c0a040e99c8899faecfdc5f08d75028
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-09-07 23:43:37 -0700

    128. Longest Consecutive Sequence https://leetcode.com/problems/longest-consecutive-sequence/ LC soln 3. Much more elegant.

--- problems/longest-consecutive-sequence/Solution.cpp
+++ problems/longest-consecutive-sequence/Solution.cpp
@@ -14,27 +14,14 @@ public:
     // consecutive sequences to find the longest consecutive sequence.
     int longestConsecutive(vector<int>& nums) {
         int count = 0;
-        unordered_set<int> us_nums, visited, um_visited;
-        unordered_map<int, int> um_nums;
+        unordered_set<int> us_nums(nums.begin(), nums.end());
 
-        for (int num : nums) us_nums.insert(num);
         for (int num : us_nums) {
-            int key = num;
-            while (visited.find(num) == visited.end() && us_nums.find(num) != us_nums.end()) {
-                visited.insert(num);
-                um_nums[key] = num - key + 1;
-                num++;
+            if (!us_nums.count(num - 1)) {
+                int key = num;
+                while (us_nums.count(num)) num++;
+                count = max(count, num - key);
             }
-        }
-        for (auto [key, value] : um_nums) {
-            int temp_k = key, temp_v = value;
-            while (um_visited.find(temp_k) == um_visited.end() && um_nums.find(temp_k + temp_v) != um_nums.end()) {
-                um_visited.insert(temp_k);
-                temp_k += temp_v;
-                temp_v = um_nums[temp_k];
-            }
-            um_nums[key] = temp_k + temp_v - key;
-            count = max(count, temp_k + temp_v - key);
         }
         return count;
     }
