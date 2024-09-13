6da7411e7b62890f13d472c79ecc248478e4881b
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-09-12 23:43:56 -0700

    959. Regions Cut By Slashes https://leetcode.com/problems/regions-cut-by-slashes/ Added comment.

--- problems/regions-cut-by-slashes/Solution.cpp
+++ problems/regions-cut-by-slashes/Solution.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 class Solution {
 public:
+    // My approach is to enumerate through the grid to find forward and backward slashes and use dfs for
+    // each encountered slash to find disjoint sets (2 boundary cells or cells/slashes that forms a loop)
+    // while keeping track of visited cells.
     int regionsBySlashes(vector<string>& grid) {
     }
 };
