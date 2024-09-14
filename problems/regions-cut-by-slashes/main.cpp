9f58d98784016633d204f84f9f0f3fe901c5fcee
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-09-14 00:22:39 -0700

    959. Regions Cut By Slashes https://leetcode.com/problems/regions-cut-by-slashes/ Fixed the driver file to process escaped backslashes in the test cases.

--- problems/regions-cut-by-slashes/main.cpp
+++ problems/regions-cut-by-slashes/main.cpp
@@ -23,9 +23,21 @@ int main(int argc, char **argv) {
 
     vector<string> grid;
     regex patternRow("[ /\\\\]+");
-    sregex_token_iterator itr(line.begin(), line.end(), patternRow);
-    for (; itr != sregex_token_iterator(); itr++) {
-        grid.push_back(*itr);
+    regex patternChar("[ /\\\\]");
+    sregex_token_iterator itrRow(line.begin(), line.end(), patternRow);
+    for (; itrRow != sregex_token_iterator(); itrRow++) {
+        string row = *itrRow;
+        sregex_token_iterator itrChar(row.begin(), row.end(), patternChar);
+        string s;
+        for (int cnt = 0; itrChar != sregex_token_iterator(); itrChar++) {
+            if (cnt % 2 == 0) s += *itrChar;
+            // Backslashes in LC input are escaped. Hence '\' is represented as '\\'.
+            // However, in `grid` passed into regionsBySlashes(), there is only 1 backslash.
+            // We need to eat the 2nd backslash in the input file so that we can use LC input
+            // as is in the test cases.
+            if (*itrChar == '\\') cnt++;
+        }
+        grid.push_back(s);
     }
 
     cout << line << ": grid" << endl;
