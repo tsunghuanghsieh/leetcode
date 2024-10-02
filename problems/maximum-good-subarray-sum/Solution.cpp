fb0b58c9d43335d9f82237db04bbdf578967059b
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-10-01 17:22:01 -0700

    3026. Maximum Good Subarray Sum https://leetcode.com/problems/maximum-good-subarray-sum/ Second iteration eliminating repeated sum calculations between nums[i] and nums[j]. The data structure used appears to incur more execution time and results in TLE error.

--- problems/maximum-good-subarray-sum/Solution.cpp
+++ problems/maximum-good-subarray-sum/Solution.cpp
@@ -7,30 +7,34 @@ using namespace std;
 
 class Solution {
 public:
-    long maximumSubarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> vals_last;
+    // Second iteration uses prefix sum to eliminate repeated sum calculations. It stil runs through
+    // all combinations of indices of nums[i] and nums[j]. However, empirical evidence on LC platform
+    // shows longer execution time using unordered_map<int, vector<int>>, vs unordered_map<int, int>.
+    // It encountered TLE error.
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int, vector<int>> vals_indices;
         unordered_set<int> seen;
         vector<int> k2 = {k, -k};
+        vector<long> prefix_sums;
         long sum = LONG_MIN;
-        // index of last occurrence of the numbers
-        for (int i = 0; i < nums.size(); i++) vals_last[nums[i]] = i;
         for (int i = 0; i < nums.size(); i++) {
-            // skipping repeated numbers
+            vals_indices[nums[i]].emplace_back(i);
+            prefix_sums.emplace_back(nums[i]);
+            if (i > 0) prefix_sums[i] += prefix_sums[i - 1];
+        }
+        for (int i = 0; i < nums.size(); i++) {
             if (seen.count(nums[i])) continue;
             seen.insert(nums[i]);
             for (int k = 0; k < 2; k++) {
-                // finding |nums[i] - nums[j]| == k
-                if (vals_last.count(nums[i] + k2[k])) {
-                    // skipping indices before i
-                    if (vals_last[nums[i] + k2[k]] < i) continue;
-                    long temp = 0;
-                    // calculating running sum between nums[j] and nums[i] (backward) where j >= i
-                    for (int last = vals_last[nums[i] + k2[k]], j = last; j >= i; j--) {
-                        temp += nums[j];
-                        // duplicated number of nums[i], update sum if running sum is larger than nums[i]
-                        if (nums[j] == nums[i] && temp > sum) sum = temp;
-                        // duplicated number of nums[j], reset temp if running sum is smaller than nums[j]
-                        if (nums[last] == nums[j] && temp < nums[last]) temp = nums[last];
+                int target = nums[i] + k2[k];
+                if (vals_indices.count(target)) {
+                    for (int ii = 0; ii < vals_indices[nums[i]].size(); ii++) {
+                        for (int j =  0; j < vals_indices[target].size(); j++) {
+                            if (vals_indices[nums[i]][ii] > vals_indices[target][j]) continue;
+                            long temp = prefix_sums[vals_indices[target][j]] -
+                                prefix_sums[vals_indices[nums[i]][ii]] + nums[i];
+                            if (temp > sum) sum = temp;
+                        }
                     }
                 }
             }
