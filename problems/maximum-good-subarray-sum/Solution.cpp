c44f2753c5083564f936741722b2e58d1930bc10
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-10-03 00:05:48 -0700

    3026. Maximum Good Subarray Sum https://leetcode.com/problems/maximum-good-subarray-sum/ Added additional comment.

--- problems/maximum-good-subarray-sum/Solution.cpp
+++ problems/maximum-good-subarray-sum/Solution.cpp
@@ -6,6 +6,18 @@ using namespace std;
 
 class Solution {
 public:
+    // The first thing to recognize for this problem should be prefix sum. The sum from nums[target] to
+    // nums[i] can then be calculated using (prefix_sum[i] - prefix_sums[seen_psums[target1]] + target),
+    // where i is the current index, target is (nums[i] +/- k), prefix_sum[i] is the prefix sum at index
+    // i and seen_psum is an unordered_map of target (key) to the index of the minimum prefix sum of
+    // target to up index i (value). Since the index of the minimum prefix sum may change over i, the sum
+    // will have to be calculated for all nums[i] using the current prefix sum, prefix_sum[i].
+    //
+    // The naive implmenetaion is to get all prefix sums in the first pass, then set the minimum prefix
+    // sum of nums[i] and the sum from nums[target] and nums[i] in the second pass. However, an improved
+    // implmenetation is to combine them in a single pass since the operations that would have been done
+    // in the second pass at index i only require output from the first pass up to index i.
+    //
     // Third iteration: runtime and space complexity O(n)
     // Instead of getting all indices of a number in the first pass and looking forward to get all
     // combinations of prefix sum of nums[i] and nums[j] in the second pass as in the second iteration,
