3c0220ca520ab24a73873878275288fdd50ea1b0
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-10-19 00:35:01 -0700

    3008. Find Beautiful Indices in the Given Array II https://leetcode.com/problems/find-beautiful-indices-in-the-given-array-ii/ Improved to pass all LC test cases.

--- problems/find-beautiful-indices-in-the-given-array-ii/Solution.cpp
+++ problems/find-beautiful-indices-in-the-given-array-ii/Solution.cpp
@@ -10,12 +10,16 @@ public:
         vector<int> res;
         int idx_s = 0, idx_b = INT_MIN;
         queue<int> q_a;
-        bool found_a, found_b;
+        bool found_a, found_b, single_a = isSingleAlpha(a), single_b = isSingleAlpha(b);
         while (idx_s < s.size()) {
-            if (found_a = find_string(s, a, idx_s); found_a) q_a.push(idx_s);
-            if (found_b = find_string(s, b, idx_s); found_b) idx_b = idx_s;
+            found_a = (idx_s + a.size() > s.size()) ? false :
+                (single_a) ? find_string(s, a, (res.size() ? res.back() : INT_MIN), idx_s) : find_string(s, a, idx_s);
+            found_b = (idx_s + b.size() > s.size()) ? false :
+                (single_b) ? find_string(s, b, idx_b, idx_s) : find_string(s, b, idx_s);
+            if (found_a) q_a.push(idx_s);
+            if (found_b) idx_b = idx_s;
             if (q_a.size() > 0 && q_a.front() + k < idx_s) q_a.pop();
-            if (idx_b + k < idx_s) idx_b = INT_MIN;
+            if (idx_b != INT_MIN && idx_b + k < idx_s) idx_b = INT_MIN;
             if ((found_a || found_b) && isWithinK(q_a, idx_b)) {
                 while (q_a.size()) {
                     res.push_back(q_a.front());
@@ -35,6 +39,19 @@ private:
             idx++;
         }
         return idx == ab.size();
+    }
+
+    bool find_string(string &s, string &ab, int prev, int idx_s) {
+        if (prev + 1 != idx_s || prev == INT_MIN) return find_string(s, ab, idx_s);
+        return (s.size() > idx_s + ab.size() - 1 && s[idx_s + ab.size() - 1] == ab[ab.size() - 1]);
+    }
+
+    bool isSingleAlpha(string s) {
+        char a = s[0];
+        for (int i = 1; i < s.size(); i++) {
+            if (s[i] != a) return false;
+        }
+        return true;
     }
 
     bool isWithinK(queue<int>& q_a, int idx_b) {
