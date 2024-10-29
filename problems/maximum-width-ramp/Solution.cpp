6a434ef03e8413834289b1e9d7f74a44651d43db
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-10-28 23:24:58 -0700

    962. Maximum Width Ramp https://leetcode.com/problems/maximum-width-ramp/ First commit of working code. Slow execution time.

--- problems/maximum-width-ramp/Solution.cpp
+++ problems/maximum-width-ramp/Solution.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <map>
 #include <vector>
 
 using namespace std;
 
 class Solution {
 public:
+    // Use map to store the number in vector as key and its indices as value. Enumerate through the map
+    // backward to find the maximum width of the ramp (see README.md for definition).
     int maxWidthRamp(vector<int>& nums) {
+        map<int, vector<int>> nummap;
+        int lastidx = -1, maxWidth = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            nummap[nums[i]].emplace_back(i);
+        }
+        for (auto itr = nummap.rbegin(); itr != nummap.rend(); itr++) {
+            lastidx = max(lastidx, (*itr).second.back());
+            if ((*itr).second.front() <= lastidx) maxWidth = max(maxWidth, lastidx - (*itr).second.front());
+        }
+
+        return maxWidth;
     }
 };
