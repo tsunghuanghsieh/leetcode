a4bacc19d1ed44546f7198535c2096fd632471f7
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-11-06 23:52:50 -0800

    432. All O`one Data Structure https://leetcode.com/problems/all-oone-data-structure/ Solution skeleton file.

---
+++ problems/all-oone-data-structure/Solution.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+class AllOne {
+public:
+    AllOne() {
+
+    }
+
+    void inc(string key) {
+
+    }
+
+    void dec(string key) {
+
+    }
+
+    string getMaxKey() {
+        return "";
+    }
+
+    string getMinKey() {
+        return "";
+    }
+};
+
+/**
+ * Your AllOne object will be instantiated and called as such:
+ * AllOne* obj = new AllOne();
+ * obj->inc(key);
+ * obj->dec(key);
+ * string param_3 = obj->getMaxKey();
+ * string param_4 = obj->getMinKey();
+ */
