e9e788f987b64f03d14c07ac306eda7ee6c2ae8a
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-11-16 02:50:25 -0800

    432. All O`one Data Structure https://leetcode.com/problems/all-oone-data-structure/ First commit of working code.

--- problems/all-oone-data-structure/Solution.cpp
+++ problems/all-oone-data-structure/Solution.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
@@ -14,20 +15,135 @@ public:
     }
 
     void inc(string key) {
-
+        Item *item;
+        if (keys.count(key) == 0) keys[key] = item = new Item(key);
+        else (item = keys[key])->size++;
+        if (item->size != 1) {
+            if (keys_freq[item->size - 1] == item) {
+                if (item->next->size == item->size - 1) keys_freq[item->size - 1] = item->next;
+                else {
+                    keys_freq.erase(item->size - 1);
+                    if (MinCount == item->size - 1) MinCount = item->size;
+                }
+            }
+            if (item->size != item->next->size) {
+                item->prev->next = item->next;
+                item->next->prev = item->prev;
+                int index = (keys_freq.count(item->size) > 0) ? item->size : item->next->size;
+                item->prev = keys_freq[index]->prev;
+                item->next = keys_freq[index];
+                keys_freq[index]->prev->next = item;
+                keys_freq[index]->prev = item;
+            }
+        }
+        else {
+            if (items != nullptr) {
+                if (keys_freq.count(item->size) == 0) {
+                    item->prev = items->prev;
+                    item->next = items;
+                    items->prev->next = item;
+                    items->prev = item;
+                    MinCount = item->size;
+                }
+                else {
+                    item->prev = keys_freq[item->size]->prev;
+                    item->next = keys_freq[item->size];
+                    keys_freq[item->size]->prev->next = item;
+                    keys_freq[item->size]->prev = item;
+                }
+            }
+        }
+        keys_freq[item->size] = item;
+        MaxCount = max(MaxCount, keys[key]->size);
+        if (MinCount == 0) MinCount = MaxCount;
+        items = keys_freq[MaxCount];
     }
 
     void dec(string key) {
-
+        Item *item = keys[key];
+        --item->size;
+        if (item->next == item) {
+            keys_freq.erase(item->size + 1);
+            if (item->size) keys_freq[item->size] = item;
+            else {
+                keys.erase(key);
+                delete item;
+                items = item = nullptr;
+            }
+            MaxCount = MinCount = (item != nullptr) ? item->size : 0;
+            return;
+        }
+        Item *temp = item->next;
+        while (temp->size > item->size) temp = temp->next;
+        if (item->size > 0) {
+            MinCount = min(MinCount, item->size);
+            if (keys_freq[item->size + 1] == item && item->next->size == item->size + 1) keys_freq[item->size + 1] = item->next;
+            if (item->next != temp) {
+                // unhook item
+                item->prev->next = item->next;
+                item->next->prev = item->prev;
+                // insert
+                item->prev = temp->prev;
+                item->next = temp;
+                temp->prev->next = item;
+                temp->prev = item;
+            }
+            keys_freq[item->size] = item;
+            if (MaxCount == item->size + 1 && keys_freq[item->size + 1] == item &&
+                item->next->size != item->size + 1) {
+                keys_freq.erase(MaxCount);
+                MaxCount = item->size;
+            }
+        }
+        else {
+            // only 1 total
+            // more than 1 at idx 1 and first one
+            // more than 1 at idx and not the first one
+            // only 1 at idx 1
+            if (item->next == item) {
+                keys_freq.erase(item->size + 1);
+                MaxCount = MinCount = 0;
+                items = nullptr;
+                keys.erase(key);
+                delete item;
+                return;
+            }
+            if (keys_freq[item->size + 1] == item) {
+                if (item->next->size != item->size + 1) {
+                    MinCount = item->prev->size;
+                    keys_freq.erase(item->size + 1);
+                }
+                else {
+                    keys_freq[item->size + 1] = item->next;
+                }
+            }
+            item->next->prev = item->prev;
+            item->prev->next = item->next;
+            keys.erase(key);
+            delete item;
+        }
     }
 
     string getMaxKey() {
-        return "";
+        return (MaxCount)? keys_freq[MaxCount]->key : "";
     }
 
     string getMinKey() {
-        return "";
+        return (MinCount) ? keys_freq[MinCount]->key : "";
     }
+private:
+    struct Item {
+        string key;
+        int size = 1;
+        Item *next, *prev;
+        Item(string s) : key(s), next(this), prev(this) {}
+    };
+
+    const string _className; // For main.cpp setup check
+    Item *items = nullptr;
+    unordered_map<string, Item*> keys;
+    unordered_map<int, Item*> keys_freq;
+    int MaxCount = 0, MinCount = 0;
 };
 
 /**
