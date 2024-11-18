7941d0414e9060ec89c9e5391a8825048bc64674
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-11-17 23:54:52 -0800

    432. All O`one Data Structure https://leetcode.com/problems/all-oone-data-structure/ Added comment.

--- problems/all-oone-data-structure/Solution.cpp
+++ problems/all-oone-data-structure/Solution.cpp
@@ -5,6 +5,11 @@ using namespace std;
 
 class AllOne {
 public:
+    // Use doubly linked list to keep track of the list of strings and their respective count.
+    // Each item contains the string, its count and links to its prev and next item(s).
+    // Unordered maps provide constant time access to items using string and count (not entirely true).
+    // At each increment, the item, newly created or existing, will be moved in the list if there exists
+    // item(s) at the incremented count or is not the first item at previous count prior to increment.
     AllOne() : _className(__func__) {
 
     }
@@ -18,17 +23,23 @@ public:
         Item *item;
         if (keys.count(key) == 0) keys[key] = item = new Item(key);
         else (item = keys[key])->size++;
-        if (item->size != 1) {
+        if (item->size != 1) {   // existing item
             if (keys_freq[item->size - 1] == item) {
+                // first and more than one items at the previous count, make the next one the first item
                 if (item->next->size == item->size - 1) keys_freq[item->size - 1] = item->next;
+                // first and only one items at the previous count, remove previous count from keys_freq
+                // update MinCount when prevous count is the MinCount
                 else {
                     keys_freq.erase(item->size - 1);
                     if (MinCount == item->size - 1) MinCount = item->size;
                 }
             }
+            // not the first item and not the only item at the prevous count
             if (item->size != item->next->size) {
+                // unhook from the previous location
                 item->prev->next = item->next;
                 item->next->prev = item->prev;
+                // insert at the new location
                 int index = (keys_freq.count(item->size) > 0) ? item->size : item->next->size;
                 item->prev = keys_freq[index]->prev;
                 item->next = keys_freq[index];
@@ -36,9 +47,10 @@ public:
                 keys_freq[index]->prev = item;
             }
         }
-        else {
+        else {   // new item created
             if (items != nullptr) {
                 if (keys_freq.count(item->size) == 0) {
+                    // count 1 has no existing item, append to the end of the list
                     item->prev = items->prev;
                     item->next = items;
                     items->prev->next = item;
@@ -46,6 +58,7 @@ public:
                     MinCount = item->size;
                 }
                 else {
+                    // insert before the first item at count 1
                     item->prev = keys_freq[item->size]->prev;
                     item->next = keys_freq[item->size];
                     keys_freq[item->size]->prev->next = item;
@@ -53,7 +66,7 @@ public:
                 }
             }
         }
-        keys_freq[item->size] = item;
+        keys_freq[item->size] = item;   // update the first item at the incremented count
         MaxCount = max(MaxCount, keys[key]->size);
         if (MinCount == 0) MinCount = MaxCount;
         items = keys_freq[MaxCount];
@@ -62,6 +75,7 @@ public:
     void dec(string key) {
         Item *item = keys[key];
         --item->size;
+        // only 1 item in the doubly linked list
         if (item->next == item) {
             keys_freq.erase(item->size + 1);
             if (item->size) keys_freq[item->size] = item;
@@ -74,9 +88,11 @@ public:
             return;
         }
         Item *temp = item->next;
+        // finding the last item of the previous count before the first item of the decrement count
         while (temp->size > item->size) temp = temp->next;
         if (item->size > 0) {
             MinCount = min(MinCount, item->size);
+            // first item and more than one item at the previous count
             if (keys_freq[item->size + 1] == item && item->next->size == item->size + 1) keys_freq[item->size + 1] = item->next;
             if (item->next != temp) {
                 // unhook item
@@ -140,7 +156,7 @@ private:
     };
 
     const string _className; // For main.cpp setup check
-    Item *items = nullptr;
+    Item *items = nullptr;   // head of doubly linked list
     unordered_map<string, Item*> keys;
     unordered_map<int, Item*> keys_freq;
     int MaxCount = 0, MinCount = 0;
