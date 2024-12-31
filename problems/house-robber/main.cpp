922af98288f2ac9e82ebfb48789bbe1dac4903c7
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2024-12-31 00:01:31 -0800

    198. House Robber https://leetcode.com/problems/house-robber/ program driver file.

---
+++ problems/house-robber/main.cpp
@@ -0,0 +1,32 @@
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <vector>
+
+using namespace std;
+
+#include "Solution.cpp"
+
+int main(int argc, char **argv) {
+    Solution soln;
+    ifstream fin;
+    fin.open(argv[1]);
+    if (!fin) {
+        cout << "ERROR: " << argv[1] << " does not exist." << endl;
+        exit(1);
+    }
+    string input;
+    int expected;
+    fin >> input >> expected;
+
+    vector<int> nums;
+    regex patternNumber("(\\d+)");
+    sregex_token_iterator itr(input.begin(), input.end(), patternNumber);
+    for (; itr != sregex_token_iterator(); itr++) nums.emplace_back(stoi(*itr));
+
+    cout << ((input.size() > 140) ?
+        (input.substr(0, 40) + "...(" +  to_string(nums.size()) + ")") : input) << ": nums" << endl;
+    cout << expected << ": expected" << endl;
+    cout << soln.rob(nums) << ": actual" << endl;
+    return 0;
+}
