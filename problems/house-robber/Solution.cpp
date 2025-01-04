4b4a97fdbdcf4bae4f753d69a679bee18e3e57d7
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2025-01-03 22:50:25 -0800

    1101. The Earliest Moment When Everyone Become Friends https://leetcode.com/problems/the-earliest-moment-when-everyone-become-friends/ First commit of working code.

--- problems/house-robber/Solution.cpp
+++ problems/house-robber/Solution.cpp
@@ -6,6 +6,12 @@ using namespace std;
 class Solution {
 public:
     int rob(vector<int>& nums) {
-
+        if (nums.size() == 1) return nums[0];
+        if (nums.size() == 2) return max(nums[0], nums[1]);
+        nums[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < nums.size(); i++) {
+            nums[i] = max(nums[i] + nums[i - 2], nums[i - 1]);
+        }
+        return nums[nums.size() - 1];
     }
 };
