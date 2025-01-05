655281d08938aebd17a90b1d34a35e2ffcc23969
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2025-01-05 00:08:52 -0800

    1101. The Earliest Moment When Everyone Become Friends https://leetcode.com/problems/the-earliest-moment-when-everyone-become-friends/ Added comment.

--- problems/house-robber/Solution.cpp
+++ problems/house-robber/Solution.cpp
@@ -5,6 +5,12 @@ using namespace std;
 
 class Solution {
 public:
+    // Initial idea was to solve it using DP. However, a pattern emerged while traversing through nums.
+    // I just need to keep track of the max sum from the beginning of nums to the current index. At each
+    // index, max sum is the max of the sum of index i and i - 2, and the sum at index i - 1. When done,
+    // the max amount is stored in index nums.size() - 1.
+    //
+    // Runtime complexity is O(n) and space complexity is O(1)
     int rob(vector<int>& nums) {
         if (nums.size() == 1) return nums[0];
         if (nums.size() == 2) return max(nums[0], nums[1]);
