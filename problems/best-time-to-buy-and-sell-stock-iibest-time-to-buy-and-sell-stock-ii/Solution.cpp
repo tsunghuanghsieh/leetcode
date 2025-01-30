c333f9d8a0f36f4a61f488200086886d671d22d5
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2025-01-29 23:55:42 -0800

    122. Best Time to Buy and Sell Stock II https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/ First commit of working code.

--- problems/best-time-to-buy-and-sell-stock-iibest-time-to-buy-and-sell-stock-ii/Solution.cpp
+++ problems/best-time-to-buy-and-sell-stock-iibest-time-to-buy-and-sell-stock-ii/Solution.cpp
@@ -6,6 +6,17 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        int sum = 0, low = prices[0], high = prices[0];
 
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] < high) {
+                sum += (high - low);
+                low = high = prices[i];
+            }
+            else if (prices[i] > high) {
+                high = prices[i];
+            }
+        }
+        return sum += (high - low);
     }
 };
