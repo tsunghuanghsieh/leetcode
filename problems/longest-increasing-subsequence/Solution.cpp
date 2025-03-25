f753e86a597b65938733cada5d458da7cc3ddbd9
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2025-03-24 18:57:41 -0700

    300. Longest Increasing Subsequence https://leetcode.com/problems/longest-increasing-subsequence/ Added comment for LC solutions.

--- problems/longest-increasing-subsequence/Solution.cpp
+++ problems/longest-increasing-subsequence/Solution.cpp
@@ -6,6 +6,22 @@ using namespace std;
 
 class Solution {
 public:
+    // LC soln #1 initializes a distance vector of size nums to 1, iterates through nums.
+    // At each index, it iterates again from index 0 to the previous index, ignoring values
+    // larger than that of the current index, finding the largest distance among the indices
+    // of smaller value, and increment the largest distance by 1 for the current index. Return
+    // the largest value in the distance vector. Time complexity is O(n^2) and space complexity
+    // is O(n).
+    //
+    // LC soln #2 iterates through nums and builds an increasing vector. If the number is
+    // larger, appends to the end of vector. Else, iterate through the increasing vector,
+    // and replace the first larger number in the vector. The size of the increasing vector
+    // is the length of the longest increasing sequence. Time complexity is O(n^2) and space
+    // complexity is O(n).
+    //
+    // LC soln #3 is same as soln #2, using binary search instead of linear search in the
+    // increasing vector. Time complexity is O(nlogn) and space complexity is O(n).
+    //
     // The worst case scenario, runtime complexity is O(n^2) and space complexity is O(n).
     int lengthOfLIS(vector<int>& nums) {
         map<int, int> order;
