e888b9159c29576db689bfc24beb999e10c3078e
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2025-06-21 16:16:30 -0700

    1984. Minimum Difference Between Highest and Lowest of K Scores https://leetcode.com/problems/minimum-difference-between-highest-and-lowest-of-k-scores/ Common infinity initializer.

--- problems/minimum-difference-between-highest-and-lowest-of-k-scores/Solution.py
+++ problems/minimum-difference-between-highest-and-lowest-of-k-scores/Solution.py
@@ -1,9 +1,8 @@
-import sys
 from typing import List
 
 class Solution:
     def minimumDifference(self, nums: List[int], k: int) -> int:
-        diff = sys.maxsize
+        diff = float('inf')
         nums.sort()
         for i in range(k - 1, len(nums)):
             diff = min(diff, nums[i] - nums[i - (k - 1)])
