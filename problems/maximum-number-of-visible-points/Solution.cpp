0bb46fb16d0d423e2d1c2f3e1f3ae18048d41e84
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2025-06-30 15:54:14 -0700

    1610. Maximum Number of Visible Points https://leetcode.com/problems/maximum-number-of-visible-points/ Improvement.

--- problems/maximum-number-of-visible-points/Solution.cpp
+++ problems/maximum-number-of-visible-points/Solution.cpp
@@ -8,6 +8,14 @@ using namespace std;
 
 class Solution {
 public:
+    // My initial approach was to initialize deque with the degrees vector as is once, which
+    // serves as the sliding window, and iterate through degrees to append to deque while
+    // looking back and popping points outside of angle from the front of deque. This results
+    // in convoluted condition statements which took a few attempts to get right.
+    //
+    // The solution found online was to duplicate the degrees with + 360 to handle circular
+    // visibility. Then use 2 pointers to indicate sliding window. Duplicating degrees (+ 360)
+    // simplifies the comparisons tremendously.
     int visiblePoints(vector<vector<int>>& points, int angle, vector<int>& location) {
         int res = 0, size = 0;
         vector<double> degrees;
@@ -23,19 +31,27 @@ public:
             degrees.emplace_back(degree);
         }
         sort(degrees.begin(), degrees.end());
+        // handle circular visibility
+        int degrees_size = degrees.size();
+        for (int i = 0; i < degrees_size; i++) degrees.emplace_back(degrees[i] + 360);
         // use sliding window
-        deque<double> window(degrees.begin(), degrees.end());
-        for (int i = 0; i < degrees.size(); i++) {
-            double deg = degrees[i];
-            while (window.size() > 0 &&
-                ((deg - angle < 0 && window.front() >= deg && window.front() < deg - angle + 360) ||
-                (deg - angle >= 0 && (window.front() >= deg || window.front() < deg - angle)))) {
-                if (i > 0 && deg == degrees[i - 1]) break;
-                window.pop_front();
-            }
-            window.emplace_back(deg);
-            size = max(size, (int) window.size());
+        for (int i = 0, j = 1; i < degrees_size; i++) {
+            while (degrees[i] + angle >= degrees[j]) j++;
+            size = max(size, j - i);
         }
+        // Initial approach
+        // deque<double> window(degrees.begin(), degrees.end());
+        // for (int i = 0; i < degrees.size(); i++) {
+        //     double deg = degrees[i];
+        //     while (window.size() > 0 &&
+        //         ((deg - angle < 0 && window.front() >= deg && window.front() < deg - angle + 360) ||
+        //         (deg - angle >= 0 && (window.front() >= deg || window.front() < deg - angle)))) {
+        //         if (i > 0 && deg == degrees[i - 1]) break;
+        //         window.pop_front();
+        //     }
+        //     window.emplace_back(deg);
+        //     size = max(size, (int) window.size());
+        // }
         return res + size;
     }
 };
