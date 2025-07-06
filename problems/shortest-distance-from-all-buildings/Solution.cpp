5c6444115c3745703e5593a7b38fd6eea5a07296
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2025-07-06 02:35:22 -0700

    317. Shortest Distance from All Buildings https://leetcode.com/problems/shortest-distance-from-all-buildings/) Optimized to use pair<int, int> for row and col, instead of encoding/decoding them to/from a single integer, by providing a hash and equal function.

--- problems/shortest-distance-from-all-buildings/Solution.cpp
+++ problems/shortest-distance-from-all-buildings/Solution.cpp
@@ -13,9 +13,12 @@ public:
     // Alternatively, it can start from an empty space, 0, to calculate distances to all buildings, 1.
     // The approach here is the latter. The speculation was there may tend to have more 0s than 1s.
     //
+    // Updated the implementation to unordered_set<pair<int, int>> by providing custom hash and equal
+    // function and eliminated conversions between 1D and 2D. The execution time dropped to ~950ms.
+    //
     // The implementation using unordered_set<int>, coverting 2D indices into 1D (row * col_size + col),
     // incurs too much translation costs during execution on big test cases individually, yet it passes
-    // all LC tests at ~2500ms.
+    // all LC tests at ~1700ms.
     //
     // NOTE: unordered_set does not have built-in hash function for vector<int> and pair<int, int>.
     //
@@ -26,40 +29,40 @@ public:
     //
     int shortestDistance(vector<vector<int>>& grid) {
         int shortest = INT_MAX, row_size = grid.size(), col_size = grid[0].size();
-        unordered_set<int> pos0s, pos1s;
+        unordered_set<pair<int, int>, hashFn, equalFn> pos0s, pos1s;
         int dirs[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};   // up, right, down, left
 
         for (int i = 0; i < row_size; i++) {
             for (int j = 0; j < col_size; j++) {
                 if (grid[i][j] == 0) {
-                    pos0s.insert(i * col_size + j);
+                    pos0s.insert(make_pair(i, j));
                     continue;
                 }
-                if (grid[i][j] == 1) pos1s.insert(i * col_size + j);
+                if (grid[i][j] == 1) pos1s.insert(make_pair(i, j));
                 grid[i][j] = INT_MAX;
             }
         }
         if (pos0s.size() == 0) return -1;
-        for (int pos : pos1s) {
+        for (auto& pos : pos1s) {
             // do bfs from each building to determine distance from all empty space in the grid
-            unordered_set<int> visited, seen1;
-            queue<int> q;
+            unordered_set<pair<int, int>, hashFn, equalFn> visited, seen1;
+            queue<pair<int, int>> q;
             q.push(pos);
             seen1.insert(pos);
             int qsize = q.size(), step = 1;
             while (qsize > 0) {
-                int val = q.front(), r = val / col_size, c = val % col_size;
+                auto val = q.front();
                 q.pop();
                 qsize--;
                 visited.insert(val);
                 for (auto dir : dirs) {
-                    int dr = r + dir[0], dc = c + dir[1], adj = dr * col_size + dc;
-                    if (pos1s.count(adj) > 0) seen1.insert(adj);
-                    if (visited.count(adj) > 0 || dr < 0 || dr >= row_size || dc < 0 || dc >= col_size) continue;
+                    int dr = val.first + dir[0], dc = val.second + dir[1], adj = dr * col_size + dc;
+                    if (pos1s.count({dr, dc}) > 0) seen1.insert({dr, dc});
+                    if (visited.count({dr, dc}) > 0 || dr < 0 || dr >= row_size || dc < 0 || dc >= col_size) continue;
                     if (grid[dr][dc] == INT_MAX) continue;
                     grid[dr][dc] += step;
-                    q.push(adj);
-                    visited.insert(adj);
+                    q.push({dr, dc});
+                    visited.insert({dr, dc});
                 }
                 if (qsize == 0) {
                     qsize = q.size();
@@ -67,17 +70,31 @@ public:
                 }
             }
             if (pos1s.size() != seen1.size()) return -1;   // some buildings are unreachable
-            for (int pos : pos0s) {
+            for (auto pos : pos0s) {
                 if (visited.count(pos) > 0) continue;
-                grid[pos / col_size][pos % col_size] = INT_MAX;   // marking unreachable empty spaces
+                grid[pos.first][pos.second] = INT_MAX;   // marking unreachable empty spaces
             }
         }
-        for (int pos : pos0s) {
-            shortest = min(shortest, grid[pos / col_size][pos % col_size]);
+        for (auto& pos : pos0s) {
+            shortest = min(shortest, grid[pos.first][pos.second]);
         }
         return (shortest != INT_MAX) ? shortest : -1;
     }
 private:
+    struct hashFn {
+        size_t operator()(const std::pair<int, int>& p) const {
+            // generating unique hash
+            return (p.first + 1) * (p.second + 1);
+        }
+    };
+
+    struct equalFn {
+        bool operator()(const std::pair<int, int>& a, const std::pair<int, int>& b) const {
+            // fulfill commutative property: equalFn(a, b) == equalFn(b, a)
+            return a.first == b.first && a.second == b.second;
+        }
+    };
+
     void printGrid(const vector<vector<int>>& grid) {
         int maxDist = 0;
         for (int r = 0; r < grid.size(); r++) {
