dad1e1af788a54b384d315a6bc0975d3bf57c7ec
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2025-07-08 23:36:03 -0700

    1296. Divide Array in Sets of K Consecutive Numbers https://leetcode.com/problems/divide-array-in-sets-of-k-consecutive-numbers/ Updated comment.

--- problems/divide-array-in-sets-of-k-consecutive-numbers/Solution.cpp
+++ problems/divide-array-in-sets-of-k-consecutive-numbers/Solution.cpp
@@ -12,8 +12,8 @@ public:
     //
     // My implementation matches LC Editorial soln #1. It uses map<int, int> to store the value of the
     // number as key and the count of occurrence as the corresponding value. It iterates through the
-    // map to decrement the count to determine if the array can be divided into sets of k consecutive
-    // numbers.
+    // map to decrement the count one (1) at a time to determine if the array can be divided into sets
+    // of k consecutive numbers.
     //
     // Runtime complexity: O(nlogn + nk) or O(nlogn) for large n, small k or O(nk) for large k, small n
     // Space complexity O(n)
