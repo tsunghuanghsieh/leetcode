2c11221edfde1041a94ab564a89aa862900e9a25
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2025-07-23 03:18:10 -0700

    886. Possible Bipartition https://leetcode.com/problems/possible-bipartition/ From deque to queue.

--- problems/possible-bipartition/Solution2.cpp
+++ problems/possible-bipartition/Solution2.cpp
@@ -10,7 +10,7 @@ public:
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
         vector<vector<int>> adj(n + 1, vector<int>());
         vector<unordered_set<int>> partitions(2);
-        deque<int> dq;
+        queue<int> q;
         for (int i = 0; i < dislikes.size(); i++) {
             adj[dislikes[i][0]].emplace_back(dislikes[i][1]);
             adj[dislikes[i][1]].emplace_back(dislikes[i][0]);
@@ -19,21 +19,21 @@ public:
         for (int i = 1, cnt = 0; i < adj.size(); i++) {
             if (partitions[0].count(i) || partitions[1].count(i)) continue;
             partitions[cnt].insert(i);
-            dq.emplace_back(i);
-            int dq_size = dq.size();
+            q.push(i);
+            int q_size = q.size();
 
-            while (dq_size) {
-                int num = dq.front();
-                dq.pop_front();
-                dq_size--;
+            while (q_size) {
+                int num = q.front();
+                q.pop();
+                q_size--;
                 for (int j = 0; j < adj[num].size(); j++) {
                     if (partitions[cnt].count(adj[num][j])) return false;
                     if (partitions[(cnt + 1) % 2].count(adj[num][j])) continue;
-                    dq.emplace_back(adj[num][j]);
+                    q.push(adj[num][j]);
                     partitions[(cnt + 1) % 2].insert(adj[num][j]);
                 }
-                if (dq_size == 0) {
-                    dq_size = dq.size();
+                if (q_size == 0) {
+                    q_size = q.size();
                     cnt = (cnt + 1) % 2;
                 }
             }
